c694dfc4ad5c033b31aa92f33bd15924490bbc95
Simon Stone <sstone1@uk.ibm.com>
2016-11-30 23:50:51 +0000

    Handle circular relationships when serializing resolved assets

--- test/serializer/jsongenerator.js
+++ test/serializer/jsongenerator.js
@@ -32,6 +32,8 @@ describe('JSONGenerator', () => {
     // let assetDeclaration1;
     let relationshipDeclaration1;
     let relationshipDeclaration2;
+    let relationshipDeclaration3;
+    let relationshipDeclaration4;
 
     before(() => {
         modelManager = new ModelManager();
@@ -60,10 +62,20 @@ describe('JSONGenerator', () => {
                 o String transactionId
                 --> MyAsset1[] myAssets
             }
+            asset SimpleAssetCircle identified by assetId {
+                o String assetId
+                --> SimpleAssetCircle next
+            }
+            asset SimpleAssetCircleArray identified by assetId {
+                o String assetId
+                --> SimpleAssetCircleArray[] next
+            }
         `);
-        // assetDeclaration1 = modelManager.getType('org.acme.MyContainerAsset1').getProperty('myAsset');
+        // assetDeclaration1 = modelManager.getType('org.acme.SimpleAssetCircle').getProperty('myAsset');
         relationshipDeclaration1 = modelManager.getType('org.acme.MyTx1').getProperty('myAsset');
         relationshipDeclaration2 = modelManager.getType('org.acme.MyTx2').getProperty('myAssets');
+        relationshipDeclaration3 = modelManager.getType('org.acme.SimpleAssetCircle').getProperty('next');
+        relationshipDeclaration4 = modelManager.getType('org.acme.SimpleAssetCircleArray').getProperty('next');
     });
 
     beforeEach(() => {
@@ -124,15 +136,48 @@ describe('JSONGenerator', () => {
             let mockResource = sinon.createStubInstance(Resource);
             mockResource.getIdentifier.returns('DOGE_1');
             mockResource.getNamespace.returns('org.acme');
+            mockResource.getFullyQualifiedIdentifier.returns('org.acme.Doge#DOGE_1');
             let options = {
                 stack: new TypedStack({}),
                 writer: mockJSONWriter,
-                modelManager: modelManager
+                modelManager: modelManager,
+                seenResources: new Set()
             };
             options.stack.push(mockResource);
             jsonGenerator.visitRelationshipDeclaration(relationshipDeclaration1, options);
             sinon.assert.calledOnce(mockJSONWriter.openObject);
             sinon.assert.calledOnce(mockJSONWriter.closeObject);
+        });
+
+        it('should serialize a circular resource if option is specified', () => {
+            jsonGenerator = new JSONGenerator(false, true);
+            let mockResource1 = sinon.createStubInstance(Resource);
+            let mockResource2 = sinon.createStubInstance(Resource);
+            let mockResource3 = sinon.createStubInstance(Resource);
+            mockResource1.getIdentifier.returns('DOGE_1');
+            mockResource1.getNamespace.returns('org.acme');
+            mockResource1.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_1');
+            mockResource1.assetId = 'DOGE_1';
+            mockResource2.getIdentifier.returns('DOGE_2');
+            mockResource2.getNamespace.returns('org.acme');
+            mockResource2.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_2');
+            mockResource2.assetId = 'DOGE_2';
+            mockResource3.getIdentifier.returns('DOGE_3');
+            mockResource3.getNamespace.returns('org.acme');
+            mockResource3.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_3');
+            mockResource3.assetId = 'DOGE_3';
+            mockResource1.next = mockResource2;
+            mockResource2.next = mockResource3;
+            mockResource3.next = mockResource1;
+            let options = {
+                stack: new TypedStack({}),
+                writer: new JSONWriter(),
+                modelManager: modelManager,
+                seenResources: new Set()
+            };
+            options.stack.push(mockResource1);
+            jsonGenerator.visitRelationshipDeclaration(relationshipDeclaration3, options);
+            options.writer.getBuffer().should.equal('"next":{"$class":"org.acme.SimpleAssetCircle","assetId":"DOGE_1","next":{"$class":"org.acme.SimpleAssetCircle","assetId":"DOGE_2","next":{"$class":"org.acme.SimpleAssetCircle","assetId":"DOGE_3","next":"DOGE_1"}}}');
         });
 
         it('should serialize an array of relationships', () => {
@@ -180,12 +225,15 @@ describe('JSONGenerator', () => {
             let mockResource2 = sinon.createStubInstance(Resource);
             mockResource1.getIdentifier.returns('DOGE_1');
             mockResource1.getNamespace.returns('org.acme');
+            mockResource1.getFullyQualifiedIdentifier.returns('org.acme.Doge#DOGE_1');
             mockResource2.getIdentifier.returns('DOGE_2');
             mockResource2.getNamespace.returns('org.acme');
+            mockResource2.getFullyQualifiedIdentifier.returns('org.acme.Doge#DOGE_2');
             let options = {
                 stack: new TypedStack({}),
                 writer: mockJSONWriter,
-                modelManager: modelManager
+                modelManager: modelManager,
+                seenResources: new Set()
             };
             options.stack.push([mockResource1, mockResource2]);
             jsonGenerator.visitRelationshipDeclaration(relationshipDeclaration2, options);
@@ -194,6 +242,37 @@ describe('JSONGenerator', () => {
             sinon.assert.calledTwice(mockJSONWriter.openObject);
             sinon.assert.calledTwice(mockJSONWriter.closeObject);
             sinon.assert.calledOnce(mockJSONWriter.closeArray);
+        });
+
+        it('should serialize a circular array of resources if option is specified', () => {
+            jsonGenerator = new JSONGenerator(false, true);
+            let mockResource1 = sinon.createStubInstance(Resource);
+            let mockResource2 = sinon.createStubInstance(Resource);
+            let mockResource3 = sinon.createStubInstance(Resource);
+            mockResource1.getIdentifier.returns('DOGE_1');
+            mockResource1.getNamespace.returns('org.acme');
+            mockResource1.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_1');
+            mockResource1.assetId = 'DOGE_1';
+            mockResource2.getIdentifier.returns('DOGE_2');
+            mockResource2.getNamespace.returns('org.acme');
+            mockResource2.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_2');
+            mockResource2.assetId = 'DOGE_2';
+            mockResource3.getIdentifier.returns('DOGE_3');
+            mockResource3.getNamespace.returns('org.acme');
+            mockResource3.getFullyQualifiedIdentifier.returns('org.acme.SimpleAssetCircle#DOGE_3');
+            mockResource3.assetId = 'DOGE_3';
+            mockResource1.next = [mockResource2, mockResource3];
+            mockResource2.next = [mockResource3, mockResource1];
+            mockResource3.next = [mockResource1, mockResource2];
+            let options = {
+                stack: new TypedStack({}),
+                writer: new JSONWriter(),
+                modelManager: modelManager,
+                seenResources: new Set()
+            };
+            options.stack.push([mockResource1, mockResource2, mockResource3]);
+            jsonGenerator.visitRelationshipDeclaration(relationshipDeclaration4, options);
+            options.writer.getBuffer().should.equal('"next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_1","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_2","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_3","next":["DOGE_1""DOGE_2"]}"DOGE_1"]},{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_3","next":["DOGE_1",{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_2","next":["DOGE_3""DOGE_1"]}]}]},{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_2","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_3","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_1","next":["DOGE_2""DOGE_3"]}"DOGE_2"]},{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_1","next":["DOGE_2",{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_3","next":["DOGE_1""DOGE_2"]}]}]},{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_3","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_1","next":[{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_2","next":["DOGE_3""DOGE_1"]}"DOGE_3"]},{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_2","next":["DOGE_3",{"$class":"org.acme.SimpleAssetCircleArray","assetId":"DOGE_1","next":["DOGE_2""DOGE_3"]}]}]}]');
         });
 
     });
