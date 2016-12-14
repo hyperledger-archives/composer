bf496fa8b440926676c0e22d585cb2990f83f242
Simon Stone <sstone1@uk.ibm.com>
2016-12-14 17:19:47 +0000

    Initial code drop

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -ev
+
+# Grab the Concerto directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+# Check that this is the right node.js version.
+if [ "${TRAVIS_NODE_VERSION}" != "" -a "${TRAVIS_NODE_VERSION}" != "4" ]; then
+    echo Not executing as not running primary node.js version.
+    exit 0
+fi
+
+# Check that this is the main repository.
+if [[ "${TRAVIS_REPO_SLUG}" != Blockchain-WW-Labs* ]]; then
+    echo "Skipping deploy; wrong repository slug."
+    exit 0
+fi
+
+# If this is not for a tagged (release) build, set the prerelease version.
+if [ -z "${TRAVIS_TAG}" ]; then
+    node ${DIR}/scripts/timestamp.js ${DIR}/package.json
+fi
+
+# Push the code to npm.
+npm publish --scope=@ibm
+
+# Push empty commits to downstream projects to trigger builds.
+REPO=`git config remote.origin.url`
+for PROJ in Concerto-System-Tests; do
+    cd ${DIR}
+    THISREPO=$(echo ${REPO} | sed "s|/[^/]*$||")/${PROJ}.git
+    rm -rf temp
+    git clone ${THISREPO} temp
+    cd temp
+    git config user.email "noreply@ibm.com"
+    git config user.name "Blockchain WW Labs - Solutions"
+    git config push.default simple
+    git commit -m "Automated commit to trigger downstream build" --allow-empty
+    git push
+done
---
+++ test/embeddedconnection.js
@@ -0,0 +1,131 @@
+/*
+ * IBM Confidential
+ * OCO Source Materials
+ * IBM Concerto - Blockchain Solution Framework
+ * Copyright IBM Corp. 2016
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ */
+
+'use strict';
+
+const BusinessNetworkDefinition = require('@ibm/ibm-concerto-common').BusinessNetworkDefinition;
+const Connection = require('@ibm/ibm-concerto-common').Connection;
+const ConnectionManager = require('@ibm/ibm-concerto-common').ConnectionManager;
+const Container = require('@ibm/ibm-concerto-runtime').Container;
+const Context = require('@ibm/ibm-concerto-runtime').Context;
+const Engine = require('@ibm/ibm-concerto-runtime').Engine;
+const SecurityContext = require('@ibm/ibm-concerto-common').SecurityContext;
+const EmbeddedConnection = require('../lib/embeddedconnection');
+
+const chai = require('chai');
+const should = chai.should();
+chai.use(require('chai-as-promised'));
+const sinon = require('sinon');
+require('sinon-as-promised');
+
+describe('EmbeddedConnection', () => {
+
+    let mockConnectionManager;
+    let mockSecurityContext;
+    let connection;
+
+    beforeEach(() => {
+        mockConnectionManager = sinon.createStubInstance(ConnectionManager);
+        mockSecurityContext = sinon.createStubInstance(SecurityContext);
+        connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1', 'org.acme.Business');
+    });
+
+    describe('#constructor', () => {
+
+        it('should construct a new connection', () => {
+            connection.should.be.an.instanceOf(Connection);
+            connection.container.should.be.an.instanceOf(Container);
+            connection.engine.should.be.an.instanceOf(Engine);
+        });
+
+    });
+
+    describe('#disconnect', () => {
+
+        it('should disconnect', () => {
+            return connection.disconnect()
+                .then(() => {
+                    should.equal(connection.container, null);
+                    should.equal(connection.engine, null);
+                });
+        });
+
+    });
+
+    describe('#login', () => {
+
+        it('should return a new security context', () => {
+            return connection.login('doge', 'suchs3cret')
+                .should.eventually.be.an.instanceOf(SecurityContext);
+        });
+
+    });
+
+    describe('#deploy', () => {
+
+        it('should call the init engine method and ping', () => {
+            let mockBusinessNetwork = sinon.createStubInstance(BusinessNetworkDefinition);
+            mockBusinessNetwork.toArchive.resolves(Buffer.from('aGVsbG8gd29ybGQ=', 'base64'));
+            mockBusinessNetwork.getName.returns('testnetwork');
+            sinon.stub(connection.engine, 'init').resolves();
+            sinon.stub(connection, 'ping').resolves();
+            return connection.deploy(mockSecurityContext, true, mockBusinessNetwork)
+                .then(() => {
+                    sinon.assert.calledOnce(connection.engine.init);
+                    sinon.assert.calledWith(connection.engine.init, sinon.match.instanceOf(Context), 'init', ['aGVsbG8gd29ybGQ=']);
+                    sinon.assert.calledOnce(connection.ping);
+                });
+        });
+
+    });
+
+    describe('#ping', () => {
+
+        it('should submit a ping query request', () => {
+            sinon.stub(connection, 'queryChainCode').resolves();
+            return connection.ping(mockSecurityContext)
+                .then(() => {
+                    sinon.assert.calledOnce(connection.queryChainCode);
+                    sinon.assert.calledWith(connection.queryChainCode, mockSecurityContext, 'ping', []);
+                });
+        });
+
+    });
+
+    describe('#queryChainCode', () => {
+
+        it('should call the engine query method', () => {
+            sinon.stub(connection.engine, 'query').resolves({ test: 'data from engine' });
+            return connection.queryChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
+                .then((result) => {
+                    sinon.assert.calledOnce(connection.engine.query);
+                    sinon.assert.calledWith(connection.engine.query, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
+                    result.should.be.an.instanceOf(Buffer);
+                    JSON.parse(result.toString()).should.deep.equal({ test: 'data from engine' });
+                });
+        });
+
+    });
+
+    describe('#invokeChainCode', () => {
+
+        it('should call the engine invoke method', () => {
+            sinon.stub(connection.engine, 'invoke').resolves({ test: 'data from engine' });
+            return connection.invokeChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
+                .then((result) => {
+                    sinon.assert.calledOnce(connection.engine.invoke);
+                    sinon.assert.calledWith(connection.engine.invoke, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
+                    should.equal(result, undefined);
+                });
+        });
+
+    });
+
+});
