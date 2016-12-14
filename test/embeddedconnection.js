70e3413ed20b6377c23163e033a3ed2e28743480
Simon Stone <sstone1@uk.ibm.com>
2016-12-14 21:36:03 +0000

    Get tests working

--- test/embeddedconnection.js
+++ test/embeddedconnection.js
@@ -13,11 +13,13 @@
 const BusinessNetworkDefinition = require('@ibm/ibm-concerto-common').BusinessNetworkDefinition;
 const Connection = require('@ibm/ibm-concerto-common').Connection;
 const ConnectionManager = require('@ibm/ibm-concerto-common').ConnectionManager;
-const Container = require('@ibm/ibm-concerto-runtime').Container;
 const Context = require('@ibm/ibm-concerto-runtime').Context;
-const Engine = require('@ibm/ibm-concerto-runtime').Engine;
-const SecurityContext = require('@ibm/ibm-concerto-common').SecurityContext;
+const DataService = require('@ibm/ibm-concerto-runtime').DataService;
 const EmbeddedConnection = require('../lib/embeddedconnection');
+const EmbeddedContainer = require('@ibm/ibm-concerto-runtime-embedded').EmbeddedContainer;
+const EmbeddedSecurityContext = require('../lib/embeddedsecuritycontext');
+const Engine = require('@ibm/ibm-concerto-runtime').Engine;
+const LoggingService = require('@ibm/ibm-concerto-runtime').LoggingService;
 
 const chai = require('chai');
 const should = chai.should();
@@ -27,22 +29,46 @@ require('sinon-as-promised');
 
 describe('EmbeddedConnection', () => {
 
+    let sandbox;
     let mockConnectionManager;
     let mockSecurityContext;
     let connection;
 
     beforeEach(() => {
+        sandbox = sinon.sandbox.create();
+        EmbeddedConnection.reset();
         mockConnectionManager = sinon.createStubInstance(ConnectionManager);
-        mockSecurityContext = sinon.createStubInstance(SecurityContext);
+        mockSecurityContext = sinon.createStubInstance(EmbeddedSecurityContext);
         connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1', 'org.acme.Business');
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('#createContainer', () => {
+
+        it('should create a new container', () => {
+            EmbeddedConnection.createContainer().should.be.an.instanceOf(EmbeddedContainer);
+        });
+
+    });
+
+    describe('#createEngine', () => {
+
+        it('should create a new engine', () => {
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            let mockLoggingService = sinon.createStubInstance(LoggingService);
+            mockContainer.getLoggingService.returns(mockLoggingService);
+            EmbeddedConnection.createEngine(mockContainer).should.be.an.instanceOf(Engine);
+        });
+
     });
 
     describe('#constructor', () => {
 
         it('should construct a new connection', () => {
             connection.should.be.an.instanceOf(Connection);
-            connection.container.should.be.an.instanceOf(Container);
-            connection.engine.should.be.an.instanceOf(Engine);
         });
 
     });
@@ -50,37 +76,125 @@ describe('EmbeddedConnection', () => {
     describe('#disconnect', () => {
 
         it('should disconnect', () => {
-            return connection.disconnect()
-                .then(() => {
-                    should.equal(connection.container, null);
-                    should.equal(connection.engine, null);
-                });
+            return connection.disconnect();
         });
 
     });
 
     describe('#login', () => {
 
-        it('should return a new security context', () => {
+        it('should return a new security context with a null chaincode ID if the business network was not specified', () => {
+            connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1');
             return connection.login('doge', 'suchs3cret')
-                .should.eventually.be.an.instanceOf(SecurityContext);
+                .then((securityContext) => {
+                    securityContext.should.be.an.instanceOf(EmbeddedSecurityContext);
+                    should.equal(securityContext.getChaincodeID(), null);
+                });
+        });
+
+        it('should return a new security context with a null chaincode ID if the business network does not exist', () => {
+            return connection.login('doge', 'suchs3cret')
+                .then((securityContext) => {
+                    securityContext.should.be.an.instanceOf(EmbeddedSecurityContext);
+                    should.equal(securityContext.getChaincodeID(), null);
+                });
+        });
+
+        it('should return a new security context with a non-null chaincode ID if the business network does exist', () => {
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            return connection.login('doge', 'suchs3cret')
+                .then((securityContext) => {
+                    securityContext.should.be.an.instanceOf(EmbeddedSecurityContext);
+                    securityContext.getChaincodeID().should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+                });
         });
 
     });
 
     describe('#deploy', () => {
 
-        it('should call the init engine method and ping', () => {
+        it('should call the init engine method, ping, and store the chaincode ID', () => {
             let mockBusinessNetwork = sinon.createStubInstance(BusinessNetworkDefinition);
             mockBusinessNetwork.toArchive.resolves(Buffer.from('aGVsbG8gd29ybGQ=', 'base64'));
             mockBusinessNetwork.getName.returns('testnetwork');
-            sinon.stub(connection.engine, 'init').resolves();
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            mockContainer.getUUID.returns('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            sandbox.stub(EmbeddedConnection, 'createContainer').returns(mockContainer);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            sandbox.stub(EmbeddedConnection, 'createEngine').returns(mockEngine);
+            mockEngine.init.resolves();
             sinon.stub(connection, 'ping').resolves();
             return connection.deploy(mockSecurityContext, true, mockBusinessNetwork)
                 .then(() => {
-                    sinon.assert.calledOnce(connection.engine.init);
-                    sinon.assert.calledWith(connection.engine.init, sinon.match.instanceOf(Context), 'init', ['aGVsbG8gd29ybGQ=']);
+                    sinon.assert.calledOnce(mockEngine.init);
+                    sinon.assert.calledWith(mockEngine.init, sinon.match.instanceOf(Context), 'init', ['aGVsbG8gd29ybGQ=']);
                     sinon.assert.calledOnce(connection.ping);
+                    sinon.assert.calledOnce(mockSecurityContext.setChaincodeID);
+                    sinon.assert.calledWith(mockSecurityContext.setChaincodeID, '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+                    EmbeddedConnection.getBusinessNetwork('testnetwork', 'devFabric1').should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+                    EmbeddedConnection.getChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f').should.deep.equal({
+                        uuid: '6eeb8858-eced-4a32-b1cd-2491f1e3718f',
+                        container: mockContainer,
+                        engine: mockEngine
+                    });
+                });
+        });
+
+    });
+
+    describe('#update', () => {
+
+        it('should update the business network', () => {
+            let mockBusinessNetwork = sinon.createStubInstance(BusinessNetworkDefinition);
+            mockBusinessNetwork.toArchive.resolves(Buffer.from('aGVsbG8gd29ybGQ=', 'base64'));
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine);
+            mockSecurityContext.getChaincodeID.returns('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            sinon.stub(connection, 'invokeChainCode').resolves();
+            return connection.update(mockSecurityContext, mockBusinessNetwork)
+                .then(() => {
+                    sinon.assert.calledOnce(connection.invokeChainCode);
+                    sinon.assert.calledWith(connection.invokeChainCode, sinon.match.instanceOf(EmbeddedSecurityContext), 'updateBusinessNetwork', ['aGVsbG8gd29ybGQ=']);
+                });
+        });
+
+    });
+
+    describe('#undeploy', () => {
+
+        it('should remove the business network', () => {
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine);
+            return connection.undeploy(mockSecurityContext, 'org.acme.Business')
+                .then(() => {
+                    should.equal(EmbeddedConnection.getBusinessNetwork('org.acme.Business', 'devFabric1'), undefined);
+                });
+        });
+
+        it('should handle a duplicate removal of a business network', () => {
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine);
+            return connection.undeploy(mockSecurityContext, 'org.acme.Business')
+                .then(() => {
+                    return connection.undeploy(mockSecurityContext, 'org.acme.Business');
                 });
         });
 
@@ -102,11 +216,19 @@ describe('EmbeddedConnection', () => {
     describe('#queryChainCode', () => {
 
         it('should call the engine query method', () => {
-            sinon.stub(connection.engine, 'query').resolves({ test: 'data from engine' });
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine);
+            mockSecurityContext.getChaincodeID.returns('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            mockEngine.query.resolves({ test: 'data from engine' });
             return connection.queryChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
                 .then((result) => {
-                    sinon.assert.calledOnce(connection.engine.query);
-                    sinon.assert.calledWith(connection.engine.query, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
+                    sinon.assert.calledOnce(mockEngine.query);
+                    sinon.assert.calledWith(mockEngine.query, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
                     result.should.be.an.instanceOf(Buffer);
                     JSON.parse(result.toString()).should.deep.equal({ test: 'data from engine' });
                 });
@@ -117,11 +239,19 @@ describe('EmbeddedConnection', () => {
     describe('#invokeChainCode', () => {
 
         it('should call the engine invoke method', () => {
-            sinon.stub(connection.engine, 'invoke').resolves({ test: 'data from engine' });
+            let mockDataService = sinon.createStubInstance(DataService);
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getDataService.returns(mockDataService);
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine);
+            mockSecurityContext.getChaincodeID.returns('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            mockEngine.invoke.resolves({ test: 'data from engine' });
             return connection.invokeChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
                 .then((result) => {
-                    sinon.assert.calledOnce(connection.engine.invoke);
-                    sinon.assert.calledWith(connection.engine.invoke, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
+                    sinon.assert.calledOnce(mockEngine.invoke);
+                    sinon.assert.calledWith(mockEngine.invoke, sinon.match.instanceOf(Context), 'testFunction', ['arg1', 'arg2']);
                     should.equal(result, undefined);
                 });
         });
