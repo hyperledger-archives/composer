302183ed6759b18750ae7bedd88ae84ae7c484f8
Simon Stone <sstone1@uk.ibm.com>
2016-12-20 20:24:13 +0000

    Improve TypeScript definition file generation

--- scripts/tsgen.js
+++ scripts/tsgen.js
@@ -17,16 +17,58 @@ const sourceModule = require('..');
 const targetFile = path.resolve(__dirname, '..', 'index.d.ts');
 
 let fileContents = '';
+
+/**
+ * Render the specified constructor.
+ * @param {string} key The name of the constructor.
+ * @param {function} clazz The constructor.
+ */
+function renderClass(key, clazz) {
+    fileContents += `export class ${key} {\n`;
+    let statics = Object.getOwnPropertyNames(clazz);
+    statics.forEach((statick) => {
+        let func = clazz[statick];
+        if (typeof func === 'function') {
+            const args = new Array(func.length).fill('temp');
+            args.forEach((value, index, array) => {
+                args[index] = `arg${index}?: any`;
+            });
+            const insert = args.join(', ');
+            fileContents += `  static ${statick}(${insert}): any;\n`;
+        }
+    });
+    let members = Object.getOwnPropertyNames(clazz.prototype);
+    members.forEach((member) => {
+        let func = clazz.prototype[member];
+        if (typeof func === 'function') {
+            const args = new Array(func.length).fill('temp');
+            args.forEach((value, index, array) => {
+                args[index] = `arg${index}?: any`;
+            });
+            const insert = args.join(', ');
+            if (member === 'constructor') {
+                fileContents += `  ${member}(${insert});\n`;
+            } else {
+                fileContents += `  ${member}(${insert}): any;\n`;
+            }
+        }
+    });
+    fileContents += '}\n';
+}
+
 if (typeof sourceModule === 'function') {
     // console.log('Source module is a function (single class)');
     let key = sourceModule.name;
-    fileContents = `declare const ${key}: any;\nexport default ${key};\n`;
+    renderClass(key, sourceModule);
+    fileContents += `export default ${key};\n`;
 } else {
     // console.log('Source module is an object (multiple or named classes)');
     let keys = Object.keys(sourceModule).sort();
     keys.forEach((key) => {
-        // console.log('Adding key', key);
-        fileContents += `export const ${key}: any;\n`;
+        if (typeof sourceModule[key] === 'function') {
+            let ctor = sourceModule[key];
+            renderClass(key, ctor);
+        }
     });
 }
 
