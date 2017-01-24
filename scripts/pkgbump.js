a804bcbe9b9d788e68d5cc493659bcc27662098a
Simon Stone <sstone1@uk.ibm.com>
2017-01-24 18:50:47 +0000

    Initial scripts to deal with problems

--- scripts/pkgbump.js
+++ scripts/pkgbump.js
@@ -16,21 +16,12 @@
 'use strict';
 
 const fs = require('fs');
-const moment = require('moment');
+const path = require('path');
+const semver = require('semver');
 
-if (process.argv.length !== 3) {
-    console.error('Usage: timestamp.js <package.json>');
-    process.exit(1);
-}
-
-let fileName = process.argv[2];
-let fileContents = fs.readFileSync(fileName, 'utf8');
-let file = JSON.parse(fileContents);
-
-let timestamp = moment().format('YYYYMMDDHHmmss');
-
-file.version = file.version.replace(/-.*/, '');
-file.version += '-' + timestamp;
-
-fileContents = JSON.stringify(file, null, 2);
-fs.writeFileSync(fileName, fileContents, 'utf8');
+const lernaDirectory = path.resolve('.');
+const lernaConfigFile = path.resolve(lernaDirectory, 'lerna.json');
+const lernaConfig = require(lernaConfigFile);
+const targetVersion = semver.inc(lernaConfig.version, 'patch');
+lernaConfig.version = targetVersion;
+fs.writeFileSync(lernaConfigFile, JSON.stringify(lernaConfig, null, 4), 'utf8');
