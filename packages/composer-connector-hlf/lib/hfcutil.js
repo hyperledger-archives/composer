f34eeb1f3542692c35b84cac85369ff3f5225cea
Daniel Selman <daniel.selman@uk.ibm.com>
2017-01-28 07:00:59 +0000

    fixed git submodules

--- packages/composer-connector-hlf/lib/hfcutil.js
+++ packages/composer-connector-hlf/lib/hfcutil.js
@@ -193,7 +193,7 @@ class HFCUtil {
             return new Promise((resolve, reject) => {
                 let sourceDirectoryPath = path.resolve(runtimeModulePath);
                 let targetDirectoryPath = path.resolve(tempDirectoryPath, 'src', chaincodePath);
-                fs.copy(sourceDirectoryPath, targetDirectoryPath, { filter: (path) => { return !/(Concerto-Runtime-Hyperledger-Fabric|concerto-runtime-hlf).*node_modules/.test(path); }}, (err) => {
+                fs.copy(sourceDirectoryPath, targetDirectoryPath, { filter: (path) => { return !/fabric-composer-runtime-hlf.*node_modules/.test(path); }}, (err) => {
                     if (err) {
                         LOG.error('deployChainCode', 'copy', err);
                         return reject(err);
