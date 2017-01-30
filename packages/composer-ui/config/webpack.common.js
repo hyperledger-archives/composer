2b3581726c8f30a0be766e6abb72718543ee35eb
Simon Stone <sstone1@uk.ibm.com>
2017-01-30 12:10:16 -0800

    Change display title in UI

--- packages/composer-ui/config/webpack.common.js
+++ packages/composer-ui/config/webpack.common.js
@@ -24,7 +24,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Blockchain Composer',
+  title: 'Fabric Composer',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
