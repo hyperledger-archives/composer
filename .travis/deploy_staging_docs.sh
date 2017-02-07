92002e8ba26ce80585028e9423f0e1f986d75716
Matthew B White <whitemat@uk.ibm.com>
2017-02-07 10:41:10 +0000

    Deploy to staging docs repo on pull requestes

--- .travis/deploy_staging_docs.sh
+++ .travis/deploy_staging_docs.sh
@@ -7,10 +7,15 @@ set -o pipefail
 # Grab the  directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then # do the full normal push
+    echo "Not deploying to staging area - merge request"
+    exit 0
+fi
+
 # Set the GitHub deploy key we will use to publish.
-set-up-ssh --key "$encrypted_f19708b15817_key" \
-           --iv "$encrypted_f19708b15817_iv" \
-           --path-encrypted-key ".travis/github_deploy_docs_key.enc"
+set-up-ssh --key "$encrypted_4664aa7e5e58_key" \
+           --iv "$encrypted_4664aa7e5e58_iv" \
+           --path-encrypted-key ".travis/github_deploy_staging_docs_key.enc"
 
 # push the html documents
 # Configure the Git repository and clean any untracked and unignored build files.
@@ -19,18 +24,18 @@ git config user.email "noreply@travis.ibm.com"
 git config push.default simple
 
 echo ${DIR}
-cd ${DIR}/site/out
+cd ${DIR}/site/out/
 
-export REPO="fabric-composer.github.io"
+export REPO="staging-fabric-composer-web"
 
 git clone git@github.com:fabric-composer/${REPO}.git
 git remote set-url origin ${REPO}.git
 
-cd ${DIR}/site/out/${REPO}
+cd ${DIR}/site/out/${REPO}/${TRAVIS_PULL_REQUEST}
 
-rm -rf ${DIR}/site/out/${REPO}/*
+#rm -rf ${DIR}/site/out/${REPO}/*
 cp -rf ${DIR}/site/out/jekylldocs/_site/* .
-
+cd ${DIR}/site/out/
 git add .
 
 git commit -m "Automated deploy to ${REPO}"
