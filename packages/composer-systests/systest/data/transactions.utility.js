f83c29c4df70f8a34f258d311434c5148c26a88c
Liam Grace <liamgrace.896@gmail.com>
2017-02-16 14:18:27 +0000

    Fixed tests to use newResource rather than newInstance

--- packages/composer-systests/systest/data/transactions.utility.js
+++ packages/composer-systests/systest/data/transactions.utility.js
@@ -17,8 +17,8 @@ function utilityFuncA(transaction) {
 function utilityFuncB(transaction) {
     return getAssetRegistry('systest.transactions.SimpleStringAsset')
         .then(function (ar) {
-            let f = getFactory();
-            let a = f.newResource('systest.transactions', 'SimpleStringAsset', 'stringAsset1');
+            var f = getFactory();
+            var a = f.newResource('systest.transactions', 'SimpleStringAsset', 'stringAsset1');
             a.stringValue = transaction.stringValue;
             return ar.add(a);
         });
