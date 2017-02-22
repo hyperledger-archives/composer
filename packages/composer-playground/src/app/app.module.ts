45eb70e7d3bcd594f6a9454faa1224d5ad7abe87
Liam Grace <liamgrace.896@gmail.com>
2017-02-22 17:03:11 +0000

    Added about section

--- packages/composer-playground/src/app/app.module.ts
+++ packages/composer-playground/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { ParticipantRegistryComponent, AddParticipantComponent, UpdateParticipan
 import { TransactionRegistryComponent, SubmitTransactionComponent } from './transactionregistry';
 import { SettingsComponent } from './settings';
 import { AddIdentityComponent } from './addidentity';
+import { AboutComponent } from './about';
 import { BusyComponent } from './busy';
 import { ErrorComponent } from './error';
 import { ResetComponent } from './reset';
@@ -41,7 +42,8 @@ import { WalletService } from './wallet.service';
 import { IdentityService } from './identity.service';
 import { NotificationService } from './notification.service';
 import { InitializationService } from './initialization.service';
-import { SampleBusinessNetworkService } from "./services/samplebusinessnetwork.service";
+import { SampleBusinessNetworkService } from './services/samplebusinessnetwork.service';
+import { AboutService } from './services/about.service';
 
 var actionBasedIcons = require.context('../assets/svg/action-based', false, /.*\.svg$/);
 actionBasedIcons.keys().forEach(actionBasedIcons);
@@ -95,7 +97,8 @@ type StoreType = {
     ResetComponent,
     ImportComponent,
     GithubComponent,
-    NoContentComponent
+    NoContentComponent,
+    AboutComponent
   ],
   imports: [ // import Angular's modules
     BrowserModule,
@@ -121,7 +124,8 @@ type StoreType = {
     IdentityService,
     NotificationService,
     InitializationService,
-    SampleBusinessNetworkService
+    SampleBusinessNetworkService,
+    AboutService
   ]
 })
 export class AppModule {
