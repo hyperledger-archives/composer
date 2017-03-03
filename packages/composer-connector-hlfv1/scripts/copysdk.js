bd47a1b0a78dfe94aa9c5bf6bc90d5a2b9a07030
Dave Kelsey <d_kelsey@uk.ibm.com>
2017-03-03 11:00:00 +0000

    change the location of the script and make gulp a dependency
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/composer-connector-hlfv1/scripts/copysdk.js
+++ packages/composer-connector-hlfv1/scripts/copysdk.js
@@ -18,18 +18,18 @@ const fs = require('fs-extra');
 
 // copy the node sdk to the expected node module directories
 // and also patch the default.json file to locate the ECDSA_AES.js file
-fs.copy('node_modules/fabric-sdk-node/fabric-ca-client', 'node_modules/fabric-ca-client', (err) => {
+fs.copy('../node_modules/fabric-sdk-node/fabric-ca-client', '../node_modules/fabric-ca-client', (err) => {
     if (err) {
         return console.error(err);
     }
 });
-fs.copy('node_modules/fabric-sdk-node/fabric-client', 'node_modules/fabric-client', (err) => {
+fs.copy('../node_modules/fabric-sdk-node/fabric-client', '../node_modules/fabric-client', (err) => {
     if (err) {
         return console.error(err);
     }
-    fs.readJson('./node_modules/fabric-client/config/default.json', (err, config) => {
+    fs.readJson('../node_modules/fabric-client/config/default.json', (err, config) => {
         config['crypto-suite-software'].EC = './impl/CryptoSuite_ECDSA_AES.js';
-        fs.writeJson('./node_modules/fabric-client/config/default.json', config, (err) => {
+        fs.writeJson('../node_modules/fabric-client/config/default.json', config, (err) => {
             if (err) {
                 return console.error(err);
             }
