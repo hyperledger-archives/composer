29f50b37e69891a7147b2ba4ad99d7d926f17d80
Simon Stone <sstone1@uk.ibm.com>
2017-03-07 21:29:35 +0000

    Restrict set of REST APIs that are generated

--- packages/composer-rest-server/server/server.js
+++ packages/composer-rest-server/server/server.js
@@ -15,42 +15,30 @@
 
 'use strict';
 
-const chalk       = require('chalk');
-const clear       = require('clear');
-const figlet      = require('figlet');
-const util        = require('../lib/util');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const util = require('../lib/util');
 const loopback = require('loopback');
 const boot = require('loopback-boot');
 const app = module.exports = loopback();
+
 process.env.SUPPRESS_NO_CONFIG_WARNING = true;
 
-app.start = function() {
-
-    // start the web server
-    return app.listen(function() {
-        app.emit('started');
-        let baseUrl = app.get('url').replace(/\/$/, '');
-        if (app.get('loopback-component-explorer')) {
-            let explorerPath = app.get('loopback-component-explorer').mountPath;
-            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-        }
-    });
-};
-
-
-const yargs = require( 'yargs' )
+const yargs = require('yargs')
     .wrap(null)
-    .usage( 'Usage: $0 -p connection profile -b business network identifier -u participant id -s participant password' )
-    .option( 'c', { alias: 'connectionProfile', describe: 'connection profile Name', type: 'string' } )
-    .option( 'b', { alias: 'businessNetwork', describe: 'business network identifier', type: 'string' } )
-    .option( 'i', { alias: 'participantId', describe: 'participant id', type: 'string' } )
-    .option( 'p', { alias: 'participantPwd', describe: 'participant password', type: 'string' } )
-    .help( 'h' )
-    .alias( 'h', 'help' )
+    .usage('Usage: $0 -p connection profile -b business network identifier -u participant id -s participant password')
+    .option('c', { alias: 'connectionProfile', describe: 'connection profile Name', type: 'string', default: process.env.COMPOSER_CONNECTION_PROFILE })
+    .option('b', { alias: 'businessNetwork', describe: 'business network identifier', type: 'string', default: process.env.COMPOSER_BUSINESS_NETWORK })
+    .option('i', { alias: 'participantId', describe: 'participant id', type: 'string', default: process.env.COMPOSER_ENROLLMENT_ID })
+    .option('p', { alias: 'participantPwd', describe: 'participant password', type: 'string', default: process.env.COMPOSER_ENROLLMENT_SECRET })
+    .help('h')
+    .alias('h', 'help')
     .argv;
 
 // see if we need to run interactively
-if(yargs.c === undefined && yargs.b === undefined && yargs.i === undefined && yargs.p === undefined) {
+let promise;
+if (yargs.c === undefined && yargs.b === undefined && yargs.i === undefined && yargs.p === undefined) {
     // Gather some args interactively
     clear();
     console.log(
@@ -59,48 +47,71 @@ if(yargs.c === undefined && yargs.b === undefined && yargs.i === undefined && ya
         )
     );
     // Get details of the server that we want to run
-    util.getFabricDetails(function(answers)  {
-        // augment the app with the extra config that we've just collected
-        let ds = {
-            'name' : 'Composer',    // not sure this matters
-            'connectionProfileName' : answers.profilename,
-            'businessNetworkIdentifier' : answers.businessNetworkId,
-            'participantId' : answers.userid,
-            'participantPwd' : answers.secret
-        };
-        app.cfg = ds;
-        boot(app, __dirname, function(err) {
-            if(err) {
-                throw err;
-            }
-            // start the server if `$ node server.js`
-            if (require.main === module) {
-                app.start();
-            }
+    promise = util.getFabricDetails()
+        .then((answers) => {
+            // augment the app with the extra config that we've just collected
+            return {
+                connectionProfileName: answers.profilename,
+                businessNetworkIdentifier: answers.businessNetworkId,
+                participantId: answers.userid,
+                participantPwd: answers.secret
+            };
         });
-    });
 
 } else {
     // make sure we have args for all required parms otherwise error
-    if(yargs.c === undefined || yargs.b === undefined || yargs.i === undefined || yargs.p === undefined) {
+    if (yargs.c === undefined || yargs.b === undefined || yargs.i === undefined || yargs.p === undefined) {
         console.log('Error: Missing parameter.   Please run compposer-rest-server -h to see usage details');
+        process.exit(1);
     } else {
-        let ds = {
-            'name' : 'Composer',    // not sure this matters
-            'connectionProfileName' : yargs.c,
-            'businessNetworkIdentifier' : yargs.b,
-            'participantId' : yargs.i,
-            'participantPwd' : yargs.p
-        };
-        app.cfg = ds;
-        boot(app, __dirname, function(err) {
-            if(err) {
-                throw err;
-            }
-            // start the server if `$ node server.js`
-            if (require.main === module) {
-                app.start();
-            }
+        promise = Promise.resolve({
+            connectionProfileName: yargs.c,
+            businessNetworkIdentifier: yargs.b,
+            participantId: yargs.i,
+            participantPwd: yargs.p
         });
     }
 }
+
+// After we have got the composer configuration...
+promise.then((composer) => {
+
+    // Store the composer configuration for the boot script to find
+    app.set('composer', composer);
+
+    // boot scripts mount components like REST API
+    return new Promise((resolve, reject) => {
+        boot(app, __dirname, (error) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve();
+        });
+    });
+
+})
+.then(() => {
+
+    app.start = function () {
+        // start the web server
+        return app.listen(function () {
+            app.emit('started');
+            let baseUrl = app.get('url').replace(/\/$/, '');
+            console.log('Web server listening at: %s', baseUrl);
+            if (app.get('loopback-component-explorer')) {
+                let explorerPath = app.get('loopback-component-explorer').mountPath;
+                console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+            }
+        });
+    };
+
+    // start the server if `$ node server.js`
+    if (require.main === module) {
+        app.start();
+    }
+
+})
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
