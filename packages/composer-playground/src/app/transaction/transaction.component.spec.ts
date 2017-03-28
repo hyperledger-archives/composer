bf41d2ea3eec42decc86e405e973241914e901c5
Liam Grace <liamgrace.896@gmail.com>
2017-03-28 10:39:33 +0100

    Transaction component unit tests

--- packages/composer-playground/src/app/transaction/transaction.component.spec.ts
+++ packages/composer-playground/src/app/transaction/transaction.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,7 +10,15 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ClientService } from './../services/client.service';
 import { InitializationService } from './../initialization.service';
 
-import { TransactionDeclaration, BusinessNetworkDefinition, Serializer, Factory, Resource } from 'composer-common';
+import {
+  TransactionDeclaration,
+  BusinessNetworkDefinition,
+  Serializer,
+  Factory,
+  Resource,
+  ModelFile,
+  Introspector
+} from 'composer-common';
 import { BusinessNetworkConnection, AssetRegistry, TransactionRegistry } from 'composer-client';
 
 import * as chai from 'chai';
@@ -33,26 +41,39 @@ fdescribe('TransactionComponent', () => {
   let mockInitializationService;
   let mockTransaction;
   let mockBusinessNetwork;
+  let mockBusinessNetworkConnection;
   let mockSerializer;
+  let mockIntrospector;
   let mockFactory;
   let mockResource;
 
   let sandbox;
 
   beforeEach(() => {
+    sandbox = sinon.sandbox.create();
     mockNgbActiveModal = sinon.createStubInstance(NgbActiveModal);
     mockClientService = sinon.createStubInstance(ClientService);
     mockInitializationService = sinon.createStubInstance(InitializationService);
     mockBusinessNetwork = sinon.createStubInstance(BusinessNetworkDefinition);
+    mockBusinessNetworkConnection = sinon.createStubInstance(BusinessNetworkConnection);
     mockSerializer = sinon.createStubInstance(Serializer);
+    mockIntrospector = sinon.createStubInstance(Introspector);
     mockFactory = sinon.createStubInstance(Factory);
     mockResource = sinon.createStubInstance(Resource);
-
+    mockClientService.getBusinessNetworkConnection.returns(mockBusinessNetworkConnection);
     mockClientService.getBusinessNetwork.returns(mockBusinessNetwork);
     mockBusinessNetwork.getSerializer.returns(mockSerializer);
-    mockSerializer.toJSON.returns({'$class': 'mock.class'});
-
+    mockBusinessNetwork.getFactory.returns(mockFactory);
+    mockBusinessNetwork.getIntrospector.returns(mockIntrospector);
+    mockSerializer.toJSON.returns({
+      '$class': 'mock.class',
+      'timestamp': 'now',
+      'transactionId': 'A'
+    });
+    mockBusinessNetworkConnection.submitTransaction = sandbox.stub();
     mockTransaction = sinon.createStubInstance(TransactionDeclaration);
+    mockNgbActiveModal.close = sandbox.stub();
+    mockInitializationService.initialize.returns(Promise.resolve());
 
     TestBed.configureTestingModule({
       imports: [
@@ -70,15 +91,57 @@ fdescribe('TransactionComponent', () => {
     });
     fixture = TestBed.createComponent(TransactionComponent);
     component = fixture.componentInstance;
-    sandbox = sinon.sandbox.create();
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
+  describe('#CodeMirror', () => {
+    it('should call the correct functions', () => {
+      let cm = {
+        foldCode: sandbox.stub(),
+        getCursor: sandbox.stub()
+      };
+
+      component['codeConfig'].extraKeys['Ctrl-Q'](cm);
+      cm.foldCode.should.be.called;
+      cm.getCursor.should.be.called;
+    });
+  });
+
   describe('#ngOnInit', () => {
 
+    it('should set transactionTypes, selectedTransaction and hiddenTransactionItems',
+    fakeAsync(() => {
+      sandbox.stub(component, 'generateTransactionDeclaration');
+      mockTransaction.isAbstract.returns(false);
+      mockIntrospector.getClassDeclarations.returns([mockTransaction]);
+      component.ngOnInit();
+      tick();
+      mockBusinessNetwork.getIntrospector.should.be.called;
+      component['transactionTypes'].length.should.equal(1);
+      component['generateTransactionDeclaration'].should.be.called;
+    }));
+
+    it('should not set transactionTypes, selectedTransaction and hiddenTransactionItems',
+    fakeAsync(() => {
+      mockIntrospector.getClassDeclarations.returns([]);
+      component.ngOnInit();
+      tick();
+      mockBusinessNetwork.getIntrospector.should.be.called;
+      component['transactionTypes'].length.should.equal(0);
+    }));
+
+    it('should not set transactionTypes when abstract class', fakeAsync(() => {
+      sandbox.stub(component, 'generateTransactionDeclaration');
+      mockTransaction.isAbstract.returns(true);
+      mockIntrospector.getClassDeclarations.returns([mockTransaction]);
+      component.ngOnInit();
+      tick();
+      mockBusinessNetwork.getIntrospector.should.be.called;
+      component['transactionTypes'].length.should.equal(0);
+    }));
   });
 
   describe('#onTransactionSelect', () => {
@@ -96,14 +159,120 @@ fdescribe('TransactionComponent', () => {
   });
 
   describe('#generateTransactionDeclaration', () => {
+    let mockModelFile;
+    beforeEach(() => {
+      mockModelFile = sinon.createStubInstance(ModelFile);
+      mockModelFile.getNamespace.returns('com.test');
+    });
 
+    it('should generate valid transaction definition', () => {
+      sandbox.stub(JSON, 'stringify');
+      component['selectedTransaction'] = mockTransaction;
+      mockTransaction.getIdentifierFieldName.returns('transactionId');
+      mockTransaction.getModelFile.returns(mockModelFile);
+      mockResource = sinon.createStubInstance(Resource);
+      component['generateTransactionDeclaration']();
+
+      mockSerializer.toJSON.should.be.called;
+      JSON.stringify.should.be.called;
+    });
+
+    it('should remove hidden transactions', () => {
+      component['selectedTransaction'] = mockTransaction;
+      mockTransaction.getIdentifierFieldName.returns('transactionId');
+      mockTransaction.getModelFile.returns(mockModelFile);
+      mockResource = sinon.createStubInstance(Resource);
+      component['hiddenTransactionItems'].set('transactionId', 'transactionId');
+      component['hiddenTransactionItems'].set('timestamp', 'transactionId');
+
+      component['generateTransactionDeclaration']();
+
+      let resourceDefenition = component['resourceDefinition'];
+      resourceDefenition = JSON.parse(resourceDefenition);
+
+      mockSerializer.toJSON.should.be.called;
+      should.not.exist(component['definitionError']);
+      should.not.exist(resourceDefenition['timestamp']);
+      should.not.exist(resourceDefenition['transactionId']);
+    });
+
+    it('should set definitionError', () => {
+      component['selectedTransaction'] = mockTransaction;
+      mockTransaction.getIdentifierFieldName.returns('transactionId');
+      mockTransaction.getModelFile.returns(mockModelFile);
+      mockResource = sinon.createStubInstance(Resource);
+      mockSerializer.toJSON = () => {
+        throw new Error();
+      };
+
+      component['generateTransactionDeclaration']();
+      component['definitionError'].should.not.be.null;
+
+    });
   });
 
   describe('#onDefinitionChanged', () => {
+    it('should validate a resource', () => {
+      mockResource.validate = sandbox.stub();
+      mockSerializer.fromJSON.returns(mockResource);
+      component['resourceDefinition'] = JSON.stringify({
+        '$class': 'mock.class',
+        'timestamp':
+        'now',
+        'transactionId': 'A'
+      });
+      component['hiddenTransactionItems'].set('transactionId', 'transactionId');
+      component['hiddenTransactionItems'].set('timestamp', 'transactionId');
 
+      component['onDefinitionChanged']();
+      should.not.exist(component['definitionError']);
+      mockResource.validate.should.be.called;
+    });
+
+    it('should set definitionError', () => {
+      mockSerializer.fromJSON = () => {
+        throw new Error();
+      };
+      component['resourceDefinition'] = JSON.stringify({
+        '$class': 'mock.class',
+        'timestamp':
+        'now',
+        'transactionId': 'A'
+      });
+      component['hiddenTransactionItems'].set('transactionId', 'transactionId');
+      component['hiddenTransactionItems'].set('timestamp', 'transactionId');
+
+      component['onDefinitionChanged']();
+      should.exist(component['definitionError']);
+    });
   });
 
   describe('#submitTransaction', () => {
-
+    it('should submit a transaction', fakeAsync(() => {
+      mockSerializer.fromJSON.returns(mockResource);
+      component['resourceDefinition'] = JSON.stringify({
+        '$class': 'mock.class',
+        'timestamp':
+        'now',
+        'transactionId': 'A'
+      });
+      component['selectedTransaction'] = mockTransaction;
+      component['submitTransaction']();
+      component['submitInProgress'].should.be.true;
+      tick();
+      tick();
+      component['submitInProgress'].should.be.false;
+      should.not.exist(component['definitionError']);
+      mockNgbActiveModal.close.should.be.called;
+    }));
   });
+
+  it('should give set definitionError', fakeAsync(() => {
+    component['resourceDefinition'] = 'error';
+    component['submitTransaction']();
+    tick();
+    tick();
+    should.exist(component['definitionError']);
+    component['submitInProgress'].should.be.false;
+  }));
 });
