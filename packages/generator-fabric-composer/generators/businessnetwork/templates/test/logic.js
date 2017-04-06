9e2c23d8b209cfd4b54c9e4159199037fedf98e7
Liam Grace <14gracel@users.noreply.github.com>
2017-04-06 11:35:50 +0100

    Added docs about skeleton business network generator (#633)
    * Added unit tests for add file component
    
    * Finished add-file component test and edit import to clear stubs
    
    * Bumped up about.service code coverage
    
    * Fixed minor linting issues
    
    * Removed fdescribe
    
    * Increase code coverage of add-file to 100%
    
    * Export AssetRegistry and ParticipantRegistry
    
    * Registry unit tests
    
    * First trasaction component test
    
    * Transaction component unit tests
    
    * Remove fdiscribe
    
    * Added resource unit tests
    
    * Github components unit tests
    
    * Added test file
    
    * Business network yeoman generator
    
    * Clean up template and give transaction template the correct name
    
    * Added missing semi colon
    
    * Added docs about skeleton business network generator
    
    * Added newlines to docs to stop md parse issues

--- packages/generator-fabric-composer/generators/businessnetwork/templates/test/logic.js
+++ packages/generator-fabric-composer/generators/businessnetwork/templates/test/logic.js
@@ -1,42 +1,27 @@
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 /**
  * Write the unit tests for your transction processor functions here
  */
 
-var AdminConnection = require('composer-admin').AdminConnection;
-var BrowserFS = require('browserfs/dist/node/index');
-var BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
-var BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
-var path = require('path');
-var fs = require('fs');
+let AdminConnection = require('composer-admin').AdminConnection;
+let BrowserFS = require('browserfs/dist/node/index');
+let BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+let BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
+let path = require('path');
+let fs = require('fs');
 
 require('chai').should();
 
-var bfs_fs = BrowserFS.BFSRequire('fs');
-var NS = '<%= namespace%>';
+let bfs_fs = BrowserFS.BFSRequire('fs');
+let NS = '<%= namespace%>';
 
 describe('#'+NS, function() {
 
-    var businessNetworkConnection;
+    let businessNetworkConnection;
 
     before(function() {
         BrowserFS.initialize(new BrowserFS.FileSystem.InMemory());
-        var adminConnection = new AdminConnection({ fs: bfs_fs });
+        let adminConnection = new AdminConnection({ fs: bfs_fs });
         return adminConnection.createProfile('defaultProfile', {
             type: 'embedded'
         })
@@ -59,16 +44,16 @@ describe('#'+NS, function() {
 
         it('should change the value property of Asset to newValue', () => {
 
-            var factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+            let factory = businessNetworkConnection.getBusinessNetwork().getFactory();
 
             // create a user
-            var user = factory.newResource(NS, 'User', '<%= appauthor%>');
+            let user = factory.newResource(NS, 'User', '<%= appauthor%>');
 
             // create the asset
-            var asset = factory.newResource(NS, 'Asset', 'ASSET_001');
+            let asset = factory.newResource(NS, 'Asset', 'ASSET_001');
             asset.value = 'old-value';
 
-            var changeAssetValue = factory.newTransaction(NS, 'ChangeAssetValue');
+            let changeAssetValue = factory.newTransaction(NS, 'ChangeAssetValue');
             changeAssetValue.relatedAsset = factory.newRelationship(NS, 'Asset', asset.$identifier);
             changeAssetValue.newValue = 'new-value';
 
