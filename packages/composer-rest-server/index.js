b62d7601baa3833014c894d27fbd0d7dd835f526
Matthew B White <whitemat@uk.ibm.com>
2017-04-06 10:23:29 +0100

    merges

--- packages/composer-rest-server/index.js
+++ packages/composer-rest-server/index.js
@@ -1,4 +1,3 @@
-
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,4 +14,4 @@
 
 'use strict';
 
-module.exports.restserver = require('./lib/server/servercmd.js');
+module.exports.restserver = require('./server/servercmd.js');
--- packages/composer-rest-server/server/servercmd.js
+++ packages/composer-rest-server/server/servercmd.js
@@ -1,13 +1,12 @@
-#!/usr/bin/env node
 /*
- * Licensed under the Apache License, Version 2.0 (the 'License');
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -15,7 +14,7 @@
 
 'use strict';
 
-const loopback = require('loopback');
+// const loopback = require('loopback');
 const server = require('./server');
 
 
@@ -38,6 +37,7 @@ function startRestServer(composer){
           app.emit('started');
           let baseUrl = app.get('url').replace(/\/$/, '');
           console.log('Web server listening at: %s', baseUrl);
+          /* istanbul ignore next */
           if (app.get('loopback-component-explorer')) {
               let explorerPath = app.get('loopback-component-explorer').mountPath;
               console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
