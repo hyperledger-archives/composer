f533f0db9b3c3f90f49971cbb86eb77b855d9f30
Jakeeyturner <jaketurner25@live.com>
2017-05-03 13:51:42 +0100

    Started to add error messages for application

--- packages/generator-fabric-composer/generators/angular/templates/src/app/configuration.ts
+++ packages/generator-fabric-composer/generators/angular/templates/src/app/configuration.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server: string = "http://localhost:3000/";
-    public ApiUrl: string = "api/";
+    public ApiIP: string = "<%= apiIP %>";
+    public ApiPort: string = "<%= apiPort %>";
+
+    public Server: string = this.ApiIP+":"+this.ApiPort;
+
+    public ApiUrl: string = "/api/";
+
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
--- packages/generator-fabric-composer/generators/angular/templates/src/app/data.service.ts
+++ packages/generator-fabric-composer/generators/angular/templates/src/app/data.service.ts
@@ -10,39 +10,39 @@ export class DataService<Type> {
     private resolveSuffix: string = '?resolve=true';
     private actionUrl: string;
     private headers: Headers;
-    
+
     constructor(private http: Http, private _configuration: Configuration) {
         this.actionUrl = _configuration.ServerWithApiUrl;
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.headers.append('Accept', 'application/json');
     }
-    
+
     public getAll(ns: string): Observable<Type[]> {
         console.log('GetAll ' + ns + ' to ' + this.actionUrl + ns);
         return this.http.get(`${this.actionUrl}${ns}`)
           .map(this.extractData)
           .catch(this.handleError);
     }
-    
+
     public getSingle(ns: string, id: string): Observable<Type> {
         console.log('GetSingle ' + ns);
-        
+
         return this.http.get(this.actionUrl + ns + '/' + id + this.resolveSuffix)
           .map(this.extractData)
           .catch(this.handleError);
     }
-    
+
     public add(ns: string, asset: Type): Observable<Type> {
         console.log('Entered DataService add');
         console.log('Add ' + ns);
         console.log('asset', asset);
-        
+
         return this.http.post(this.actionUrl + ns, asset)
           .map(this.extractData)
           .catch(this.handleError);
     }
-    
+
     public update(ns: string, id: string, itemToUpdate: Type): Observable<Type> {
         console.log('Update ' + ns);
         console.log('what is the id?', id);
@@ -52,15 +52,15 @@ export class DataService<Type> {
           .map(this.extractData)
           .catch(this.handleError);
     }
-    
+
     public delete(ns: string, id: string): Observable<Type> {
         console.log('Delete ' + ns);
-        
+
         return this.http.delete(this.actionUrl + ns + '/' + id)
           .map(this.extractData)
           .catch(this.handleError);
     }
-    
+
     private handleError(error: any): Observable<string> {
         // In a real world app, we might use a remote logging infrastructure
         // We'd also dig deeper into the error to get a better message
@@ -69,9 +69,9 @@ export class DataService<Type> {
         console.error(errMsg); // log to console instead
         return Observable.throw(errMsg);
     }
-    
+
     private extractData(res: Response): any {
         return res.json();
     }
-    
+
 }
