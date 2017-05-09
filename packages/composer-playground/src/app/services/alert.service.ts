05fb39c15edf00f46fe9caf74de784503f504ce3
Caroline Church <caroline.church@uk.ibm.com>
2017-05-09 14:12:30 +0100

    Update busy component
    Updated to remove ok button and it will close when finished
    
    contributes to hyperledger/composer#839

--- packages/composer-playground/src/app/services/alert.service.ts
+++ packages/composer-playground/src/app/services/alert.service.ts
@@ -6,6 +6,6 @@ export class AlertService {
 
     // TODO think about not exposing this directly
     public errorStatus$: Subject<string> = new BehaviorSubject<string>(null);
-    public busyStatus$: Subject<string> = new BehaviorSubject<string>(null);
+    public busyStatus$: Subject<any> = new BehaviorSubject<any>(null);
     public successStatus$: Subject<string> = new BehaviorSubject<string>(null);
 }
