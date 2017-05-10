f6ac442430c8d979ad6797601ce11d02111d5e70
Simon Stone <sstone1@uk.ibm.com>
2017-05-11 00:19:37 +0100

    Remove uuencode/uudecode

--- packages/composer-website/installers/hlfv0.6/build.sh
+++ packages/composer-website/installers/hlfv0.6/build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 cp -f install.sh.in install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - docker-compose.yml | uuencode - >> install.sh
+tar czf - docker-compose.yml >> install.sh
