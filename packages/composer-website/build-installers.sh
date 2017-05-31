120fd6c3cb150c780e152beacce0f9101ab5d94d
Matthew B White <whitemat@uk.ibm.com>
2017-05-31 09:11:23 +0100

    updated docker network name

--- packages/composer-website/build-installers.sh
+++ packages/composer-website/build-installers.sh
@@ -7,7 +7,8 @@ set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/installers"
 
-curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
+#curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
+curl -O https://raw.githubusercontent.com/mbwhite/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
 
 
 # Build all of the installers.
@@ -17,4 +18,9 @@ do
     unzip fabric-dev-servers.zip -d $i/fabric-dev-servers/
     $i/build.sh
     cp -f $i/install.sh ../jekylldocs/install-$i.sh
+    rm -rf $i/fabric-dev-servers/
 done
+
+# clean up
+rm fabric-dev-servers.zip
+ 
