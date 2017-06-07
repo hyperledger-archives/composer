e62dcf532442eed7094e4de51a4baa41b32e366b
Caroline Church <caroline.church@uk.ibm.com>
2017-06-07 09:57:55 +0100

    Fix expand resources
    Fixed animation
    Fixed problem with only showing first 1000px of height

--- packages/composer-playground/src/app/directives/check-overflow/check-overflow.directive.ts
+++ packages/composer-playground/src/app/directives/check-overflow/check-overflow.directive.ts
@@ -1,10 +1,19 @@
-import { Directive, ElementRef, EventEmitter, Output, Input } from '@angular/core';
+import {
+    Directive,
+    ElementRef,
+    EventEmitter,
+    Output,
+    Input,
+    Renderer,
+    ContentChild,
+    AfterViewInit
+} from '@angular/core';
 
 @Directive({
     selector: '[checkOverFlow]',
 })
 
-export class CheckOverFlowDirective {
+export class CheckOverFlowDirective implements AfterViewInit {
 
     @Output()
     public hasOverFlow: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -17,16 +26,46 @@ export class CheckOverFlowDirective {
         }
     }
 
-    private _thing = null;
+    @Input()
+    set expanded(expanded) {
+        this._expanded = expanded;
+        this.checkOverFlow();
+    }
 
-    constructor(private el: ElementRef) {
+    @ContentChild('resourcedata') preEl: ElementRef;
+
+    private _thing = null;
+    private _expanded = false;
+
+    constructor(private el: ElementRef, private renderer: Renderer) {
+    }
+
+    ngAfterViewInit() {
+        this.checkOverFlow();
     }
 
     checkOverFlow() {
         let element = this.el;
+        let preElement = this.preEl;
+
+        if (!preElement) {
+            return;
+        }
+
+        let expanded = this._expanded;
+
+        if (expanded) {
+            let contentHeight = preElement.nativeElement.scrollHeight;
+            this.renderer.setElementStyle(preElement.nativeElement, 'maxHeight', contentHeight + 'px');
+        } else {
+            this.renderer.setElementStyle(preElement.nativeElement, 'maxHeight', '100px');
+        }
+
         setTimeout(() => {
-            let overFlowAmount = element.nativeElement.scrollHeight - element.nativeElement.offsetHeight;
-            if (overFlowAmount > 10) {
+
+            let scrollHeight = preElement.nativeElement.scrollHeight;
+
+            if (expanded || scrollHeight > 110) {
                 this.hasOverFlow.emit(true);
             } else {
                 this.hasOverFlow.emit(false);
