c9d77c29d79fe5c9cedc906923fc93de7fedc2af
Dave Kelsey <d_kelsey@uk.ibm.com>
2017-06-08 17:24:02 +0100

    restore lost code
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/composer-website/installers/hlfv1/build.sh
+++ packages/composer-website/installers/hlfv1/build.sh
@@ -5,8 +5,8 @@ cd "${DIR}"
 #cp -f install.sh.in install.sh
 cat install.sh.in | sed 's/{{ENV}}//g' > install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - crypto-config docker-compose.yml docker-compose-playground.yml fabric-dev-servers mychannel.tx twoorgs.orderer.block >> install.sh
+tar czf - docker-compose-playground.yml fabric-dev-servers mychannel.tx twoorgs.genesis.block >> install.sh
 
 cat install.sh.in | sed 's/{{ENV}}/-unstable/g' > install-unstable.sh
 echo "PAYLOAD:" >> install-unstable.sh
-tar czf - crypto-config docker-compose.yml docker-compose-playground-unstable.yml fabric-dev-servers mychannel.tx twoorgs.orderer.block >> install-unstable.sh
+tar czf - docker-compose-playground-unstable.yml fabric-dev-servers mychannel.tx twoorgs.genesis.block >> install-unstable.sh
