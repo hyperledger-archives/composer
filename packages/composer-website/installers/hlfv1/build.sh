adee651a7354e5797b0d21f210502306631bcfda
Matthew B White <whitemat@uk.ibm.com>
2017-06-08 21:41:39 +0100

    update scipts

--- packages/composer-website/installers/hlfv1/build.sh
+++ packages/composer-website/installers/hlfv1/build.sh
@@ -2,7 +2,7 @@
 set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
-#cp -f install.sh.in install.sh
+
 cat install.sh.in | sed 's/{{ENV}}//g' > install.sh
 echo "PAYLOAD:" >> install.sh
 tar czf - crypto-config docker-compose.yml docker-compose-playground.yml fabric-dev-servers mychannel.tx twoorgs.orderer.block >> install.sh
