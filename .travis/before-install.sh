7215037652200c89cd4a8a0f020fbd303f6d2384
Dan Selman <danscode@selman.org>
2017-06-13 00:39:24 +0100

    update docker

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -14,11 +14,13 @@ echo "Docker-compose version: "
 docker-compose --version
 
 # Update docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-trusty main'
 sudo apt-get update
-sudo apt-get purge docker-engine
-sudo apt-get autoremove --purge docker-engine
+sudo apt-get remove docker docker-engine
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 sudo apt-get install docker-ce
 echo "Docker version: " 
 docker --version
