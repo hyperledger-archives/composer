594a6ca4257ada6486203ccbc0d4dacb8ba317ec
Travis CI User <travis@example.org>
2017-06-21 01:15:20 +0000

    Automatic deployment of website

--- unstable/composer-data/fabric-dev-servers/fabric-scripts/hlfv0.6/README.md
+++
@@ -1,1 +0,0 @@
-## Script to setup Hyperledger Fabric 0.6
--- unstable/composer-data/fabric-dev-servers/startFabric.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/_loader.sh
-
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#npm install --prefix "${DIR}"
-
-#source "${DIR}"/_loader.sh
