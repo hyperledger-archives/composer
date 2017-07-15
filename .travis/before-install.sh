76006cd75a5acb74cf63df347ad062b29593de24
Simon Stone <sstone1@uk.ibm.com>
2017-07-15 22:56:51 +0100

    Combine npm/pip installs into one line

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -7,19 +7,13 @@ set -o pipefail
 # Install using pip as apt-get pulls the wrong version on Travis' trusty image
 # python requests 2.9.2 is essential prereq for linkchecker
 
-sudo pip install linkchecker
-sudo pip install linkchecker --upgrade
-sudo pip install requests==2.9.2
+pip install linkchecker requests==2.9.2
 linkchecker --version
 
 # Grab the parent (root) directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-npm install -g npm@4
-npm install -g lerna@2
-npm install -g @alrra/travis-scripts
-
-npm install -g asciify
+npm install -g npm@4 lerna@2 @alrra/travis-scripts asciify
 
 
 echo "ABORT_BUILD=false" > ${DIR}/build.cfg
