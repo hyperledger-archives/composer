28606da9abfeb2aa3ffd7d48da13cd79d1a1d9a3
caroline-church <cazfletch@gmail.com>
2017-08-10 14:02:16 +0100

    Use id cards with identity registry (#1828)
    Issue Identity uses id card
    IdentityComponent uses id card
    
    contributes to hyperledger/composer#1799

--- packages/composer-playground/src/app/editor/editor-file/editor-file.component.spec.ts
+++ packages/composer-playground/src/app/editor/editor-file/editor-file.component.spec.ts
@@ -102,6 +102,20 @@ describe('EditorFileComponent', () => {
         });
     });
 
+    describe('set previewReadmeActive', () => {
+        it('should set the preview boolean to true', () => {
+            component['_previewReadmeActive'] = false;
+            component.previewReadmeActive = true;
+            component['_previewReadmeActive'].should.be.true;
+        });
+
+        it('should set the preview boolean to false', () => {
+            component['_previewReadmeActive'] = true;
+            component.previewReadmeActive = false;
+            component['_previewReadmeActive'].should.be.false;
+        });
+    });
+
     describe('loadFile', () => {
         it('should load a model file', () => {
             mockClientService.getModelFile.returns({
