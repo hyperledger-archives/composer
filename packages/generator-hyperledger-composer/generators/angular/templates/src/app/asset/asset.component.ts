5c275287be0fc902f2ee1c9479b5877b033763df
Peter Ilfrich <peterilf@au1.ibm.com>
2017-09-06 15:27:31 +1000

    Added API documentation for the asset angualar template helpers/event handlers (enums)

--- packages/generator-hyperledger-composer/generators/angular/templates/src/app/asset/asset.component.ts
+++ packages/generator-hyperledger-composer/generators/angular/templates/src/app/asset/asset.component.ts
@@ -65,7 +65,11 @@ export class <%= currentAsset.name %>Component implements OnInit {
         }
     });
   }
-
+	/**
+   * Event handler for changing the checked state of a checkbox (handles array enumeration values)
+	 * @param {String} name - the name of the asset field to update
+	 * @param {any} value - the enumeration value for which to toggle the checked state
+   */
   changeArrayValue(name: string, value: any): void {
     const index = this[name].value.indexOf(value);
     if (index === -1) {
@@ -75,12 +79,18 @@ export class <%= currentAsset.name %>Component implements OnInit {
     }
   }
 
+	/**
+	 * Checkbox helper, determining whether an enumeration value should be selected or not (for array enumeration values
+	 * only). This is used for checkboxes in the asset updateDialog.
+	 * @param {String} name - the name of the asset field to check
+	 * @param {any} value - the enumeration value to check for
+	 * @return {Boolean} whether the specified asset field contains the provided value
+	 */
   hasArrayValue(name: string, value: any): boolean {
     return this[name].value.indexOf(value) !== -1;
   }
 
   addAsset(form: any): Promise<any> {
-
     this.asset = {
       $class: "<%= namespace %>.<%= currentAsset.name %>",
       <% for(var x=0;x<currentAsset.properties.length;x++){ %>
