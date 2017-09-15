afa5437718a2fb8fbe04ed7bacf8317252761874
Matthew B White <whitemat@uk.ibm.com>
2017-09-15 16:11:13 +0100

    remove some more commented out test code

--- packages/composer-runtime/test/engine.businessnetworks.js
+++ packages/composer-runtime/test/engine.businessnetworks.js
@@ -119,66 +119,6 @@ describe('EngineBusinessNetworks', () => {
         });
     });
 
-    // describe('#updateBusinessNetwork', () => {
-
-    //     it('should throw for invalid arguments', () => {
-    //         let result = engine.invoke(mockContext, 'updateBusinessNetwork', ['no', 'args', 'supported']);
-    //         return result.should.be.rejectedWith(/Invalid arguments "\["no","args","supported"\]" to function "updateBusinessNetwork", expecting "\[\"businessNetworkArchive\"\]"/);
-    //     });
-
-    //     it('should update the business network archive and create default registries', () => {
-    //         let sysdata = sinon.createStubInstance(DataCollection);
-    //         sysdata.update.withArgs('businessnetwork', { data: 'aGVsbG8gd29ybGQ=' }).resolves();
-    //         mockDataService.getCollection.withArgs('$sysdata').resolves(sysdata);
-    //         let mockBusinessNetworkDefinition = sinon.createStubInstance(BusinessNetworkDefinition);
-    //         let mockScriptManager = sinon.createStubInstance(ScriptManager);
-    //         mockBusinessNetworkDefinition.getScriptManager.returns(mockScriptManager);
-    //         sandbox.stub(BusinessNetworkDefinition, 'fromArchive').resolves(mockBusinessNetworkDefinition);
-    //         let mockScriptCompiler = sinon.createStubInstance(ScriptCompiler);
-    //         let mockCompiledScriptBundle = sinon.createStubInstance(CompiledScriptBundle);
-    //         mockScriptCompiler.compile.returns(mockCompiledScriptBundle);
-    //         mockContext.getScriptCompiler.returns(mockScriptCompiler);
-    //         let mockQueryCompiler = sinon.createStubInstance(QueryCompiler);
-    //         let mockCompiledQueryBundle = sinon.createStubInstance(CompiledQueryBundle);
-    //         mockQueryCompiler.compile.returns(mockCompiledQueryBundle);
-    //         mockContext.getQueryCompiler.returns(mockQueryCompiler);
-    //         let mockAclCompiler = sinon.createStubInstance(AclCompiler);
-    //         let mockCompiledAclBundle = sinon.createStubInstance(CompiledAclBundle);
-    //         mockAclCompiler.compile.returns(mockCompiledAclBundle);
-    //         mockContext.getAclCompiler.returns(mockAclCompiler);
-    //         sandbox.stub(Context, 'cacheBusinessNetwork');
-    //         sandbox.stub(Context, 'cacheCompiledScriptBundle');
-    //         sandbox.stub(Context, 'cacheCompiledQueryBundle');
-    //         sandbox.stub(Context, 'cacheCompiledAclBundle');
-    //         mockRegistryManager.createDefaults.resolves();
-    //         return engine.invoke(mockContext, 'updateBusinessNetwork', ['aGVsbG8gd29ybGQ='])
-    //             .then((result) => {
-    //                 sinon.assert.calledOnce(sysdata.update);
-    //                 sinon.assert.calledWith(sysdata.update, 'businessnetwork', { data: 'aGVsbG8gd29ybGQ=', hash: 'dc9c1c09907c36f5379d615ae61c02b46ba254d92edb77cb63bdcc5247ccd01c' });
-    //                 sinon.assert.calledOnce(Context.cacheBusinessNetwork);
-    //                 sinon.assert.calledWith(Context.cacheBusinessNetwork, 'dc9c1c09907c36f5379d615ae61c02b46ba254d92edb77cb63bdcc5247ccd01c', mockBusinessNetworkDefinition);
-    //                 sinon.assert.calledOnce(Context.cacheCompiledScriptBundle);
-    //                 sinon.assert.calledWith(Context.cacheCompiledScriptBundle, 'dc9c1c09907c36f5379d615ae61c02b46ba254d92edb77cb63bdcc5247ccd01c', mockCompiledScriptBundle);
-    //                 sinon.assert.calledOnce(Context.cacheCompiledQueryBundle);
-    //                 sinon.assert.calledWith(Context.cacheCompiledQueryBundle, 'dc9c1c09907c36f5379d615ae61c02b46ba254d92edb77cb63bdcc5247ccd01c', mockCompiledQueryBundle);
-    //                 sinon.assert.calledOnce(Context.cacheCompiledAclBundle);
-    //                 sinon.assert.calledWith(Context.cacheCompiledAclBundle, 'dc9c1c09907c36f5379d615ae61c02b46ba254d92edb77cb63bdcc5247ccd01c', mockCompiledAclBundle);
-    //                 sinon.assert.calledTwice(mockContext.initialize);
-    //                 // Initialize.
-    //                 sinon.assert.calledWith(mockContext.initialize);
-    //                 // Reinitialize.
-    //                 sinon.assert.calledWith(mockContext.initialize, {
-    //                     businessNetworkDefinition: mockBusinessNetworkDefinition,
-    //                     compiledScriptBundle: mockCompiledScriptBundle,
-    //                     compiledQueryBundle: mockCompiledQueryBundle,
-    //                     compiledAclBundle: mockCompiledAclBundle,
-    //                     reinitialize: true
-    //                 });
-    //                 sinon.assert.calledOnce(mockRegistryManager.createDefaults);
-    //             });
-    //     });
-
-    // });
 
     describe('#resetBusinessNetwork', () => {
 
