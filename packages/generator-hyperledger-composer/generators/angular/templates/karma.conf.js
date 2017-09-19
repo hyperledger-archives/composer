20fdb77d7fc56d50375832bdb3525e18e25da995
Krishna Harsha Voora <krishvoor@in.ibm.com>
2017-09-19 16:05:34 -0400

    Fix  #2180
    Updating relevant package.json files to pick up karma-firefox-launcher
    and ensure testing is done on Firefox browsers which is available across
    all architecture
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>

--- packages/generator-hyperledger-composer/generators/angular/templates/karma.conf.js
+++ packages/generator-hyperledger-composer/generators/angular/templates/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     singleRun: false
   });
 };
