08f2b64bce2a7b86487a02d9ea3be074efed5b12
Matthew B White <whitemat@uk.ibm.com>
2017-10-30 09:05:49 +0000

    create class index, and do test updates to a single class
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- packages/composer-admin/lib/adminconnection.js
+++ packages/composer-admin/lib/adminconnection.js
@@ -129,9 +129,20 @@ class AdminConnection {
             const locationName = card.getBusinessNetworkName() || card.getConnectionProfile().name;
             name = card.getUserName() + '@' + locationName;
         }
-        return this.cardStore.put(name, card).then(() => {
-            return name;
-        });
+        let connectionProfileData;
+        return this.cardStore.put(name, card)
+            .then(() => {
+                connectionProfileData = card.getConnectionProfile();
+                return this.connectionProfileManager.getConnectionManagerByType(connectionProfileData.type);
+            })
+            .then((connectionManager)=>{
+                let certificate = card.getCredentials().certificate;
+                let privateKey = card.getCredentials().privateKey;
+                return connectionManager.importIdentity(connectionProfileData.name,connectionProfileData, name.getUserName(), certificate, privateKey);
+            })
+            .then( ()=>{
+                return name;
+            });
     }
 
     /**
