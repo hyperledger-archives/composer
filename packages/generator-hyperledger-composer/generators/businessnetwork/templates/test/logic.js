20791dd2ac618fda947ddc36abeddae9a8e3d426
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2017-11-15 17:52:03 +0000

    Use new connect API in Yo generated business network unit tests (#2733)
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- packages/generator-hyperledger-composer/generators/businessnetwork/templates/test/logic.js
+++ packages/generator-hyperledger-composer/generators/businessnetwork/templates/test/logic.js
@@ -3,89 +3,111 @@
  * Write the unit tests for your transction processor functions here
  */
 
-var AdminConnection = require('composer-admin').AdminConnection;
-var BrowserFS = require('browserfs/dist/node/index');
-var BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
-var BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
-var path = require('path');
-var fs = require('fs');
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
+const IdCard = require('composer-common').IdCard;
+const MemoryCardStore = require('composer-common').MemoryCardStore;
+
+const path = require('path');
 
 require('chai').should();
 
-var bfs_fs = BrowserFS.BFSRequire('fs');
-var NS = '<%= namespace%>';
+const NS = '<%= namespace%>';
+const assetType = 'SampleAsset';
 
-var assetType = 'SampleAsset';
+describe('#' + NS, () => {
+    let businessNetworkConnection;
 
-describe('#'+NS, function() {
-
-    var businessNetworkConnection;
-
-    before(function() {
-        BrowserFS.initialize(new BrowserFS.FileSystem.InMemory());
-        var adminConnection = new AdminConnection({ fs: bfs_fs });
-        return adminConnection.createProfile('defaultProfile', {
+    before(() => {
+        // Embedded connection used for local testing
+        const connectionProfile = {
+            name: 'embedded',
             type: 'embedded'
-        })
-        .then(function() {
-            return adminConnection.connectWithDetails('defaultProfile', 'admin', 'Xurw3yU9zI0l');
-        })
-        .then(function() {
+        };
+        // Embedded connection does not need real credentials
+        const credentials = {
+            certificate: 'FAKE CERTIFICATE',
+            privateKey: 'FAKE PRIVATE KEY'
+        };
+
+        // Identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+
+        // Identity used to connect to business networks
+        const userMetadata = {
+            version: 1,
+            userName: 'admin',
+            businessNetwork: '<%= appname%>'
+        };
+        const userCard = new IdCard(userMetadata, connectionProfile);
+        userCard.setCredentials(credentials);
+
+        const deployerCardName = 'deployer';
+        const userCardName = 'user';
+
+        // In-memory card store for testing so cards are not persisted to the file system
+        const cardStore = new MemoryCardStore();
+        const adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        return adminConnection.importCard(deployerCardName, deployerCard).then(() => {
+            return adminConnection.importCard(userCardName, userCard);
+        }).then(() => {
+            return adminConnection.connect(deployerCardName);
+        }).then(() => {
             return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
-        })
-        .then(function(businessNetworkDefinition) {
+        }).then(businessNetworkDefinition => {
             return adminConnection.deploy(businessNetworkDefinition);
-        })
-        .then(function() {
-            businessNetworkConnection = new BusinessNetworkConnection({ fs: bfs_fs });
-            return businessNetworkConnection.connectWithDetails('defaultProfile', '<%= appname%>', 'admin', 'Xurw3yU9zI0l');
+        }).then(() => {
+            businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+            return businessNetworkConnection.connect(userCardName);
         });
     });
 
-    describe('ChangeAssetValue()', function() {
-
+    describe('ChangeAssetValue()', () => {
         it('should change the value property of ' + assetType + ' to newValue', () => {
+            const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
 
-            var factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+            // Create a user participant
+            const user = factory.newResource(NS, 'User', '<%= appauthor%>');
 
-            // create a user
-            var user = factory.newResource(NS, 'User', '<%= appauthor%>');
-
-            // create the asset
-            var asset = factory.newResource(NS, assetType, 'ASSET_001');
+            // Create the asset
+            const asset = factory.newResource(NS, assetType, 'ASSET_001');
             asset.value = 'old-value';
 
-            var changeAssetValue = factory.newTransaction(NS, 'ChangeAssetValue');
+            // Create a transaction to change the asset's value property
+            const changeAssetValue = factory.newTransaction(NS, 'ChangeAssetValue');
             changeAssetValue.relatedAsset = factory.newRelationship(NS, assetType, asset.$identifier);
             changeAssetValue.newValue = 'new-value';
 
-            // Get the asset registry.
-            return businessNetworkConnection.getAssetRegistry(NS + '.' + assetType)
-            .then(function(registry) {
+            let assetRegistry;
 
-                // Add the Asset to the asset registry.
-                return registry.add(asset)
-                .then(function() {
-                    return businessNetworkConnection.getParticipantRegistry(NS + '.User');
-                })
-                .then(function(userRegistry) {
-                    return userRegistry.add(user);
-                })
-                .then(function() {
-                    // submit the transaction
-                    return businessNetworkConnection.submitTransaction(changeAssetValue);
-                })
-                .then(function() {
-                    return businessNetworkConnection.getAssetRegistry(NS + '.' + assetType);
-                })
-                .then(function(registry) {
-                    // get the listing
-                    return registry.get(asset.$identifier);
-                })
-                .then(function(newAsset) {
-                    newAsset.value.should.equal('new-value');
-                });
+            return businessNetworkConnection.getAssetRegistry(NS + '.' + assetType).then(registry => {
+                assetRegistry = registry;
+                // Add the asset to the appropriate asset registry
+                return registry.add(asset);
+            }).then(() => {
+                return businessNetworkConnection.getParticipantRegistry(NS + '.User');
+            }).then(userRegistry => {
+                // Add the user to the appropriate participant registry
+                return userRegistry.add(user);
+            }).then(() => {
+                // Submit the transaction
+                return businessNetworkConnection.submitTransaction(changeAssetValue);
+            }).then(registry => {
+                // Get the asset
+                return assetRegistry.get(asset.$identifier);
+            }).then(newAsset => {
+                // Assert that the asset has the new value property
+                newAsset.value.should.equal(changeAssetValue.newValue);
             });
         });
     });
+
 });
