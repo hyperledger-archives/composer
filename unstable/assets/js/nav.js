5eb9e0a0927d22859c2394226023aea336d5f9e0
Travis CI User <travis@example.org>
2017-11-15 17:18:14 +0000

    Automatic deployment of website

--- unstable/assets/js/nav.js
+++ unstable/assets/js/nav.js
@@ -1,13 +1,50 @@
+var openRotation = 270;
+var closeRotation = 180;
 
 $(function(){
     $('a').each(function(){
         if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); $(this).closest('li').addClass('active');
+            $(this).addClass('active');
+            $(this).closest('li').addClass('active');
             $(this).parent().parent().parent().addClass('active');
         }
     });
 });
 
+$(function(){
+    $('.context-nav a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).parent().parent().css('background-color', 'white');
+            $(this).parent().parent().parent().css('background-color', 'white');
+        }
+    });
+});
+
+$(function(){
+    $('.caret').each(function(){
+        $(this).rotate(closeRotation);
+        if ($(this).closest('li').hasClass('active')) {
+            $(this).rotate(openRotation);
+        }
+        if ($(this).closest('li').children('ul').length == 0) {
+            $(this).toggleClass('hidden');
+        }
+    });
+});
+
+$('.caret').click(function() {
+    if ($(this).closest('li').hasClass('hide')){
+        $(this).rotate(openRotation);
+        $(this).closest('li').toggleClass('hide');
+        return false;
+    } else {
+        if (!$(this).closest('li').hasClass('active')) {
+            $(this).rotate(closeRotation);
+        }
+        $(this).closest('li').toggleClass('hide');
+        return false;
+    }
+});
 
 $(function(){
     $('.context-nav > ul > li').each(function(){
@@ -16,22 +53,19 @@ $(function(){
     });
 });
 
-
+$(function(){
+    $('.caret').each(function(){
+        $(this).addClass('hide');
+    });
+});
 
 var timer;
-var delay = 250;
+var delay = 0;
 
-$('.context-nav > ul > li').each(function(){
-      var themenuitem = $(this); // necessary to get 'this' to work for some reason
-      $(this).hover(function() {
-          // on mouse in, start a timeout
-          timer = setTimeout(function() {
-              themenuitem.removeClass('hide');
-              // do your stuff here
-          }, delay);
-      }, function() {
-          // on mouse out, cancel the timer
-          clearTimeout(timer);
-          themenuitem.addClass('hide');
-      });
-  });
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
