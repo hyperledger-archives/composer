858629e5ff9bbc1cde294419ec47f65003c60781
Matthew B White <whitemat@uk.ibm.com>
2017-12-15 08:42:52 +0000

    simplify scripts
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

---
+++ .travis/base.sh
@@ -0,0 +1,63 @@
+## this is a script soley for inclusion in the main build scripts. 
+## Common functions that are executed both all types of builds
+
+ME=`basename "$0"`
+echo "-->-- Starting ${ME}"
+echo "--I-- ${TRAVIS_TAG} ${TRAVIS_BRANCH}"
+
+function _exit(){
+    printf "%s Exiting %s because %s exit code:%s\n" "--<--" "${ME}" "$1" "$2"   
+    exit $2
+}
+
+function _abortBuild(){
+    echo "ABORT_BUILD=true" > ${DIR}/build.cfg
+    echo "ABORT_CODE=$" >> ${DIR}/build.cfg
+    echo "BUILD_FOCUS=${BUILD_FOCUS}" >> ${DIR}/build.cfg
+    echo "BUILD_RELEASE=${BUILD_RELEASE}" >> ${DIR}/build.cfg
+}
+
+#check to see if the build.cfg file (that holds state between scripts)
+if [ ! -f ${DIR}/build.cfg ]; then
+
+    echo "ABORT_BUILD=false" > ${DIR}/build.cfg
+    echo "ABORT_CODE=0" >> ${DIR}/build.cfg
+    ## regexp to match the latest version
+    LATEST_REGEXP=v0\.16\.\([0-9]{1,2}\|x\)
+    NEXT_REGEXP=v0\.17\.\([0-9]{1,2}\|x\)
+
+    ## determine the build type here
+    if [ -z "${TRAVIS_TAG}" ]; then
+        if [ "${TRAVIS_BRANCH}" = "master" ]; then
+            BUILD_FOCUS="next"
+            BUILD_RELEASE="unstable"
+        elif [[ "${TRAVIS_BRANCH}" =~ ${LATEST_REGEXP} ]]; then
+            BUILD_FOCUS="latest"
+            BUILD_RELEASE="unstable"
+        else 
+            _exit "unable to determine build focus ${TRAVIS_BRANCH} ${TRAVIS_TAG}" 1
+        fi
+    else
+        if [[ "${TRAVIS_BRANCH}" =~ ${NEXT_REGEXP} ]]; then
+            BUILD_FOCUS="next"
+            BUILD_RELEASE="stable"
+        elif [[ "${TRAVIS_BRANCH}" =~ ${LATEST_REGEXP} ]]; then
+            BUILD_FOCUS="latest"
+            BUILD_RELEASE="stable"
+        else 
+            _exit "unable to determine build focus ${TRAVIS_BRANCH} ${TRAVIS_TAG}" 1
+        fi
+    fi
+
+    echo "BUILD_FOCUS=${BUILD_FOCUS}" >> ${DIR}/build.cfg
+    echo "BUILD_RELEASE=${BUILD_RELEASE}" >> ${DIR}/build.cfg
+fi
+
+source ${DIR}/build.cfg
+
+echo "--I-- Build focus is ${BUILD_FOCUS}"
+echo "--I-- Build release is ${BUILD_RELEASE}"
+
+if [ "${ABORT_BUILD}" = "true" ]; then
+  _exit "exiting early from" ${ABORT_CODE}
+fi
--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -4,8 +4,10 @@
 set -ev
 set -o pipefail
 
-# Grab the parent (root) directory.
+# Bring in the standard set of script utilities
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+source ${DIR}/.travis/base.sh
+# ----
 
 # Install using pip as apt-get pulls the wrong version on Travis' trusty image
 # python requests 2.9.2 is essential prereq for linkchecker
@@ -15,30 +17,19 @@ linkchecker --version
 
 npm install -g lerna@2 @alrra/travis-scripts asciify gnomon
 
-echo "ABORT_BUILD=false" > ${DIR}/build.cfg
-echo "ABORT_CODE=0" >> ${DIR}/build.cfg
-
 # Abort the fv/integration if this is a merge build
 # Check for the FC_TASK that is set in travis.yml, also the pull request is false => merge build
 # and that the TRAVIS_TAG is empty meaning this is not a release build
 if [ "${FC_TASK}" = "systest" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ]; then
   if [[ "${TRAVIS_REPO_SLUG}" = hyperledger* ]]; then
-    echo "ABORT_BUILD=true" > ${DIR}/build.cfg
-    echo "ABORT_CODE=0" >> ${DIR}/build.cfg
-    echo Merge build from non release PR: ergo not running fv/integration tests
-    exit 0
+    _abortBuild 0
+    _exit "Merge build from non release PR: ergo not running fv/integration tests"   
   fi
 fi
 
-#
-echo "->- Build cfg being used"
-cat ${DIR}/build.cfg
-echo "-<-"
-
 # Check of the task current executing
 if [ "${FC_TASK}" = "docs" ]; then
-  echo Doing Docs - no requirement for installations of other software
-  exit 0;
+ _exit "Doing Docs - no requirement for installations of other software" 0
 fi
 
 #
@@ -49,3 +40,5 @@ sudo dpkg -i google-chrome*.deb
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update && sudo apt-get install cf-cli
+
+_exit "All Complete" 0
