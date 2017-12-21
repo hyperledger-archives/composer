76abae03df5200a6c0bd87100213367dc7c7ecbd
Daniel Selman <danscode@selman.org>
2017-12-21 18:00:38 +0000

    Added the ability to use the PlantUML visitor with a ModelManager

--- packages/composer-common/test/codegen/plantumlvisitor.js
+++ packages/composer-common/test/codegen/plantumlvisitor.js
@@ -45,6 +45,25 @@ describe('PlantUMLVisitor', function(){
             fs.accessSync('./out/animaltracking/model.uml', fs.F_OK);
         });
 
+        it('should generate PlantUML code from the ModelManager for animaltracking BusinessNetworkDefinition', function() {
+
+            const animaltrackingModel = fs.readFileSync(path.resolve(__dirname, '../data/model/animaltracking.cto'), 'utf8');
+            const animaltrackingScript = fs.readFileSync(path.resolve(__dirname, '../data/model/animaltracking.cto.js'), 'utf8');
+
+            // create and populate the ModelManager with a model file
+            const businessNetworkDefinition = new BusinessNetworkDefinition('com-ibm-composer-animaltracking-defranetwork@1.0.0', 'DEFRA Animal Tracking Network');
+            businessNetworkDefinition.getModelManager().addModelFile(animaltrackingModel,'animaltracking.cto');
+            const script = businessNetworkDefinition.getScriptManager().createScript('animaltracking.cto.js', 'JS', animaltrackingScript);
+            businessNetworkDefinition.getScriptManager().addScript(script);
+
+            let visitor = new PlantUMLVisitor();
+            let parameters = {};
+            parameters.fileWriter = new FileWriter('./out/animaltracking');
+            businessNetworkDefinition.getModelManager().accept(visitor, parameters);
+
+            // check the file exists
+            fs.accessSync('./out/animaltracking/model.uml', fs.F_OK);
+        });
 
         it('coverage for random object',function(){
 
