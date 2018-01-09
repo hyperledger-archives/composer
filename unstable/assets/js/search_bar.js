5c8048cfb8a6a81170db46f8eac10be0ef985437
Travis CI User <travis@example.org>
2018-01-09 15:34:42 +0000

    Automatic deployment of website

--- unstable/assets/js/search_bar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.trigger').parent().on('click',function() {
-    $('.search-box').addClass('opened');
-    $('.submit-input').removeClass('hide');
-    $('.search-icon').addClass('hide');
-    $('.search-box').focus();
-  });
-
-});
