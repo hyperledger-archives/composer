8c9db466ae1c27841d26e9dfc31127469d268220
Ellis Henderson <ellis.henderson@ibm.com>
2018-03-21 15:58:14 +0000

    Additional code to allow for participants
    Signed-off-by: Ellis Henderson <ellis.henderson@ibm.com>

--- packages/generator-hyperledger-composer/generators/angular/templates/src/app/app.module.ts
+++ packages/generator-hyperledger-composer/generators/angular/templates/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HomeComponent } from './home/home.component';
 <% for(var x=0;x<assetComponentNames.length;x++){ %>
 import { <%= assetComponentNames[x] %> } from './<%= assetList[x].name %>/<%= assetList[x].name %>.component';<% } %>
 
+<% for(var x=0;x<participantComponentNames.length;x++){ %>
+  import { <%= participantComponentNames[x] %> } from './<%= participantList[x].name %>/<%= participantList[x].name %>.component';<% } %>
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,11 @@ import { <%= assetComponentNames[x] %> } from './<%= assetList[x].name %>/<%= as
     // TransactionComponent,
     <% for(var x=0;x<assetComponentNames.length;x++){ %><% if(x == assetComponentNames.length-1){ %>
     <%= assetComponentNames[x] %><%}else{%><%= assetComponentNames[x] %>,<% } %>
-		<% } %>
+    <% } %>,
+    
+    <% for(var x=0;x<participantComponentNames.length;x++){ %><% if(x == participantComponentNames.length-1){ %>
+      <%= participantComponentNames[x] %><%}else{%><%= participantComponentNames[x] %>,<% } %>
+      <% } %>
   ],
   imports: [
     BrowserModule,
