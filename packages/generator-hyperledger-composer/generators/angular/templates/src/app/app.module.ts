c87257b34cb6f52994e6c1ebf17d431fa81160ad
K. L. Usher <kai.usher@ibm.com>
2018-03-26 16:56:37 +0100

    Angular changes
    Signed-off-by: K. L. Usher <kai.usher@ibm.com>

--- packages/generator-hyperledger-composer/generators/angular/templates/src/app/app.module.ts
+++ packages/generator-hyperledger-composer/generators/angular/templates/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { Configuration }     from './configuration';
 import { DataService }     from './data.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-// import { TransactionComponent } from './Transaction/Transaction.component'
 <% for(var x=0;x<assetComponentNames.length;x++){ %>
 import { <%= assetComponentNames[x] %> } from './<%= assetList[x].name %>/<%= assetList[x].name %>.component';<% } %>
 
+<% for(var x=0;x<participantComponentNames.length;x++){ %>
+  import { <%= participantComponentNames[x] %> } from './<%= participantList[x].name %>/<%= participantList[x].name %>.component';<% } %>
+
+<% for(var x=0;x<transactionComponentNames.length;x++){ %>
+  import { <%= transactionComponentNames[x] %> } from './<%= transactionList[x].name %>/<%= transactionList[x].name %>.component';<% } %>  
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +22,15 @@ import { <%= assetComponentNames[x] %> } from './<%= assetList[x].name %>/<%= as
     // TransactionComponent,
     <% for(var x=0;x<assetComponentNames.length;x++){ %><% if(x == assetComponentNames.length-1){ %>
     <%= assetComponentNames[x] %><%}else{%><%= assetComponentNames[x] %>,<% } %>
-		<% } %>
+    <% } %>,
+    
+    <% for(var x=0;x<participantComponentNames.length;x++){ %><% if(x == participantComponentNames.length-1){ %>
+      <%= participantComponentNames[x] %><%}else{%><%= participantComponentNames[x] %>,<% } %>
+      <% } %>,
+
+    <% for(var x=0;x<transactionComponentNames.length;x++){ %><% if(x == transactionComponentNames.length-1){ %>
+        <%= transactionComponentNames[x] %><%}else{%><%= transactionComponentNames[x] %>,<% } %>
+        <% } %>  
   ],
   imports: [
     BrowserModule,
