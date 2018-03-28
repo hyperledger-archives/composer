66199d4bb9d19a33bb91f172af8481d133b20278
Simon Stone <sstone1@uk.ibm.com>
2018-03-29 00:01:55 +0100

    .sh files
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- .travis/fix_github_https_repo.sh
+++ .travis/fix_github_https_repo.sh
@@ -1,4 +1,18 @@
 #/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #-- Script to automate https://help.github.com/articles/why-is-git-always-asking-for-my-password
 
 REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
