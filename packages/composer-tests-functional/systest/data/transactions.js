1e003878103cf2cd272dd1c384b64f1f40303484
Simon Stone <sstone1@uk.ibm.com>
2018-03-29 00:25:01 +0100

    .js files
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- packages/composer-tests-functional/systest/data/transactions.js
+++ packages/composer-tests-functional/systest/data/transactions.js
@@ -1,10 +1,24 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*eslint no-var: 0*/
 'use strict';
 
 // this function doesn't do anything but it ensures that the runtime doesn't
 // try to create an object with references to it's functions if it did you
 // would get ReferenceError: s is not defined
-var require2=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"json-logic-js":[function(require,module,exports){
+var require2=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require==='function'&&require;if(!u&&a){return a(o,!0);}if(i){return i(o,!0);}var f=new Error('Cannot find module \''+o+'\'');throw f.code='MODULE_NOT_FOUND',f;}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e);},l,l.exports,e,t,n,r);}return n[o].exports;}var i=typeof require==='function'&&require;for(var o=0;o<r.length;o++){s(r[o]);}return s;})({'json-logic-js':[function(require,module,exports){
 },{}]},{},[]);
 
 
