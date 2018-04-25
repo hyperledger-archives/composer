f6ae19e8ab0348cb34d7cd7a243b7b4e85d89e8c
caroline-church <cazfletch@gmail.com>
2018-04-25 15:05:12 +0100

    Update Busy Status (#3917)
    Removed new progress dialog
    Updated message to show progress on deploy and upgrade
    
    contributes to hyperledger/composer#3780
    
    Signed-off-by: Caroline Church <caroline.church@uk.ibm.com>

--- packages/composer-playground/src/app/basic-modals/busy/busy.component.spec.ts
+++ packages/composer-playground/src/app/basic-modals/busy/busy.component.spec.ts
@@ -16,41 +16,23 @@
 /* tslint:disable:no-var-requires */
 /* tslint:disable:max-classes-per-file */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, DebugElement, Input } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
-import * as chai from 'chai';
 import * as sinon from 'sinon';
-
-let should = chai.should();
 
 import { BusyComponent } from './busy.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { IdCard } from 'composer-common';
-
-@Component({
-    selector: 'identity-card',
-    template: '<section class="identity-card"></section>'
-})
-class MockIdentityCardComponent {
-    @Input() identity: any;
-    @Input() preview: boolean;
-}
-
 @Component({
     template: `
-        <busy [busy]="busy" [header]="header" [card]="card"></busy>`
+        <busy [busy]="busy"></busy>`
 })
 class TestHostComponent {
-    busy: any = {
+    busy = {
         title: 'myTitle',
         text: 'myText'
     };
-
-    header = null;
-
-    card = null;
 }
 
 describe('BusyComponent', () => {
@@ -63,7 +45,7 @@ describe('BusyComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [BusyComponent, TestHostComponent, MockIdentityCardComponent],
+            declarations: [BusyComponent, TestHostComponent],
             providers: [{provide: NgbActiveModal, useValue: mockActiveModal}]
         });
 
@@ -79,7 +61,6 @@ describe('BusyComponent', () => {
 
     it('should display the error', () => {
         fixture.detectChanges();
-        let headerSection = busyElement.query(By.css('.modal-header'));
         let titleElement = busyElement.queryAll(By.css('span'));
         titleElement[1].nativeElement.innerHTML.should.equal('myTitle');
 
@@ -93,74 +74,6 @@ describe('BusyComponent', () => {
 
         fixture.detectChanges();
         titleElement[1].nativeElement.innerHTML.should.equal('differentTitle');
-
         textElement[0].nativeElement.innerHTML.should.equal('differentText');
-
-        should.not.exist(headerSection);
-    });
-
-    it('should display a header if there is one', () => {
-        component.header = 'myHeader';
-        fixture.detectChanges();
-        let headerSection = busyElement.query(By.css('.modal-header'));
-        let headerHeading = headerSection.query(By.css('h1'));
-        headerHeading.nativeElement.textContent.should.equal('myHeader');
-
-        let titleElement = busyElement.queryAll(By.css('span'));
-        titleElement[1].nativeElement.innerHTML.should.equal('myTitle');
-
-        let textElement = busyElement.queryAll(By.css('.busy-text'));
-        textElement[0].nativeElement.innerHTML.should.equal('myText');
-    });
-
-    it('should display a header if there is one', () => {
-        component.header = 'myHeader';
-        fixture.detectChanges();
-        let headerContainer = busyElement.query(By.css('.modal-header'));
-        let headerElement = headerContainer.query(By.css('h1'));
-        headerElement.nativeElement.innerHTML.should.equal('myHeader');
-
-        let titleElement = busyElement.queryAll(By.css('span'));
-        titleElement[1].nativeElement.innerHTML.should.equal('myTitle');
-
-        let textElement = busyElement.queryAll(By.css('.busy-text'));
-        textElement[0].nativeElement.innerHTML.should.equal('myText');
-    });
-
-    it('should display an identity card if there is one', () => {
-        component.header = 'myHeader';
-        component.card = sinon.createStubInstance(IdCard);
-
-        fixture.detectChanges();
-
-        let titleContainer = busyElement.query(By.css('.busy-title'));
-        let titleElement = titleContainer.query(By.css('h2'));
-        titleElement.nativeElement.innerHTML.should.equal('myTitle');
-
-        let textElement = busyElement.query(By.css('.busy-text'));
-        textElement.nativeElement.innerHTML.should.equal('myText');
-
-        let cardElement = busyElement.query(By.css('.identity-card'));
-        should.exist(cardElement);
-    });
-
-    it('should display an identity card and progress if there is any', () => {
-        component.busy.progress = [ 'myProgress' ];
-        component.header = 'myHeader';
-        component.card = sinon.createStubInstance(IdCard);
-
-        fixture.detectChanges();
-
-        let titleContainers = busyElement.queryAll(By.css('.busy-title'));
-        let progressElement = titleContainers[0].query(By.css('h2'));
-        progressElement.nativeElement.innerHTML.should.equal('myProgress');
-        let titleElement = titleContainers[1].query(By.css('h2'));
-        titleElement.nativeElement.innerHTML.should.equal('myTitle');
-
-        let textElement = busyElement.query(By.css('.busy-text'));
-        textElement.nativeElement.innerHTML.should.equal('myText');
-
-        let cardElement = busyElement.query(By.css('.identity-card'));
-        should.exist(cardElement);
     });
 });
