aac4db822ac6a26526d10e836c62b00ccb21befa
Dave Kelsey <d_kelsey@uk.ibm.com>
2018-05-09 17:38:17 +0100

    Improve Embedded Connector Identity Management
    closes #2857
    
    This PR includes the ability to host the embedded connector in a server via the composer-connector-server and drive it from client applications such as CLI or Rest Server. It will probably also work with playground but only if running locally.
    
    Integration tests would be required + docs to make this an officially supported capability so for now, it won’t be documented.
    
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/composer-connector-embedded/test/embeddedconnection.js
+++ packages/composer-connector-embedded/test/embeddedconnection.js
@@ -15,7 +15,7 @@
 'use strict';
 
 const { BusinessNetworkDefinition, Certificate, Connection, ConnectionManager } = require('composer-common');
-const { Context, DataCollection, DataService, Engine, LoggingService } = require('composer-runtime');
+const { Context, DataCollection, DataService, Engine, LoggingService, InstalledBusinessNetwork } = require('composer-runtime');
 const EmbeddedContainer = require('composer-runtime-embedded').EmbeddedContainer;
 const EmbeddedConnection = require('../lib/embeddedconnection');
 const EmbeddedSecurityContext = require('../lib/embeddedsecuritycontext');
@@ -52,6 +52,7 @@ describe('EmbeddedConnection', () => {
             issuer: 'ce295bc0df46512670144b84af55f3d9a3e71b569b1e38baba3f032dc3000665',
             secret: 'suchsecret',
             certificate: '',
+            imported: false,
             options: {
                 issuer: true
             }
@@ -102,18 +103,13 @@ describe('EmbeddedConnection', () => {
 
     describe('#createTransactionId', () => {
         it('should just return null ', ()=>{
-            connection.createTransactionId(mockSecurityContext)
-            .then((result)=>{
-                should.be.equal(result,null);
-            }
-            );
+            connection.createTransactionId(mockSecurityContext).should.eventually.be.null;
         });
     });
 
     describe('#login', () => {
 
         beforeEach(() => {
-            sandbox.stub(connection, 'testIdentity').resolves();
         });
 
         it('should return a new security context with a null chaincode ID if the business network was not specified', async () => {
@@ -127,28 +123,68 @@ describe('EmbeddedConnection', () => {
         });
 
         it('should throw if the business network was specified but it does not exist', () => {
+            sandbox.stub(connection, 'testIdentity').resolves();
             return connection.login('doge', 'suchs3cret')
                 .should.be.rejectedWith(/No chaincode ID found/);
         });
 
         it('should return a new security context with a non-null chaincode ID if the business network does exist', async () => {
             EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            identity.imported = true;
+            sandbox.stub(connection, 'testIdentity').resolves(identity);
             let securityContext = await connection.login('doge', 'suchs3cret');
             securityContext.should.be.an.instanceOf(EmbeddedSecurityContext);
             securityContext.getChaincodeID().should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
             sinon.assert.calledWith(connection.testIdentity, 'doge', 'suchs3cret');
         });
 
+        it('should simulate enrolment and import of credentials if no credentials imported and has a business network', async () => {
+            connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1', 'org.acme.Business');
+            EmbeddedConnection.addBusinessNetwork('org.acme.Business', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            let updatedIdentity = {};
+            Object.assign(updatedIdentity, identity);
+            updatedIdentity.imported = true;
+            let mockIdentitiesDataCollection = sinon.createStubInstance(DataCollection);
+            sandbox.stub(connection, 'testIdentity').resolves(identity);
+            sandbox.stub(connection, 'getIdentities').resolves(mockIdentitiesDataCollection);
+            mockIdentitiesDataCollection.get.withArgs('doge').resolves(identity);
+            await connection.login('doge', 'suchs3cret');
+            sinon.assert.calledOnce(mockIdentitiesDataCollection.update);
+            sinon.assert.calledWith(mockIdentitiesDataCollection.update.firstCall, 'doge', updatedIdentity);
+            //sinon.assert.calledWith(mockIdentitiesDataCollection.update.secondCall, updatedIdentity.identifier, updatedIdentity);
+        });
+
+        it('should not simulate enrolment if credentials already imported', async () => {
+            connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1');
+            let updatedIdentity = {};
+            Object.assign(updatedIdentity, identity);
+            updatedIdentity.imported = true;
+            let mockIdentitiesDataCollection = sinon.createStubInstance(DataCollection);
+            sandbox.stub(connection, 'testIdentity').resolves(updatedIdentity);
+            sandbox.stub(connection, 'getIdentities').resolves(mockIdentitiesDataCollection);
+            mockIdentitiesDataCollection.get.withArgs('doge').resolves(updatedIdentity);
+            await connection.login('doge', 'suchs3cret');
+            sinon.assert.notCalled(mockIdentitiesDataCollection.update);
+        });
+
+
     });
 
     describe('#install', ()  => {
         it('should set the business network definition for the connection and return a resolved promise', async () => {
             await connection.install(mockSecurityContext, businessNetworkDefinition);
-            connection.businessNetworkDefinition.should.equal(businessNetworkDefinition);
+            EmbeddedConnection.getInstalledChaincode(businessNetworkDefinition.getName(), businessNetworkDefinition.getVersion()).should.equal(businessNetworkDefinition);
         });
     });
 
     describe('#start', () => {
+        it('should throw an error if chaincode has not been installed', () => {
+            connection.start(mockSecurityContext,
+                businessNetworkDefinition.getName(),
+                businessNetworkDefinition.getVersion(),
+                '{"start":"json"}',
+                { start: 'options' }).should.be.rejectedWith(/has not been installed/);
+        });
 
         it('should call the init engine method, ping, and store the chaincode ID', async () => {
             // Mock a container
@@ -182,13 +218,70 @@ describe('EmbeddedConnection', () => {
             sinon.assert.calledOnce(mockEngine.init);
             sinon.assert.calledWith(mockEngine.init, sinon.match.instanceOf(Context), 'start', ['{"start":"json"}']);
             EmbeddedConnection.getBusinessNetwork(businessNetworkDefinition.getName(), 'devFabric1').should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
-            EmbeddedConnection.getChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f').should.deep.equal({
-                uuid: '6eeb8858-eced-4a32-b1cd-2491f1e3718f',
-                container: mockContainer,
-                engine: mockEngine,
-                installedBusinessNetwork: connection.installedBusinessNetwork
-            });
+
+            const chaincode = EmbeddedConnection.getChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+
+            // can't use deep equal here due to a cyclic object which causes a hang.
+            chaincode.uuid.should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            chaincode.container.should.equal(mockContainer);
+            chaincode.engine.should.equal(mockEngine);
+            chaincode.installedBusinessNetwork.should.be.instanceOf(InstalledBusinessNetwork);
+
         });
+    });
+
+    describe('#upgrade', () => {
+        it('should throw an error if chaincode has not been installed', () => {
+            connection.upgrade(mockSecurityContext,
+                businessNetworkDefinition.getName(),
+                businessNetworkDefinition.getVersion()).should.be.rejectedWith(/has not been installed/);
+        });
+
+        it('should throw an error if chaincode has not been started', () => {
+            EmbeddedConnection.addInstalledChaincode(businessNetworkDefinition);
+            connection.upgrade(mockSecurityContext,
+                businessNetworkDefinition.getName(),
+                businessNetworkDefinition.getVersion()).should.be.rejectedWith(/has not been started/);
+        });
+
+        it('should upgrade the chaincode', async () => {
+            EmbeddedConnection.addInstalledChaincode(businessNetworkDefinition);
+            EmbeddedConnection.addBusinessNetwork('digitalproperty-network', 'devFabric1', '6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            // Mock a container
+            let mockContainer = sinon.createStubInstance(EmbeddedContainer);
+            mockContainer.getUUID.returns('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            sandbox.stub(EmbeddedConnection, 'createContainer').returns(mockContainer);
+
+            // Mock an engine
+            let mockEngine = sinon.createStubInstance(Engine);
+            mockEngine.getContainer.returns(mockContainer);
+            mockEngine.init.resolves();
+            sandbox.stub(EmbeddedConnection, 'createEngine').returns(mockEngine);
+
+            const mockInstalledBusinessNetwork = sinon.createStubInstance(InstalledBusinessNetwork);
+            sandbox.stub(InstalledBusinessNetwork, 'newInstance').resolves(mockInstalledBusinessNetwork);
+
+            // put something weird for installedBusinessNetwork so we can detect it has changed.
+            EmbeddedConnection.addChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f', mockContainer, mockEngine, 'orgInstalledBusinessNetwork');
+
+            // Mock a securty context
+            mockSecurityContext.getIdentity.returns(identity);
+
+            await connection.upgrade(mockSecurityContext, businessNetworkDefinition.getName(),
+            businessNetworkDefinition.getVersion());
+
+            sinon.assert.calledOnce(mockEngine.init);
+            sinon.assert.calledWith(mockEngine.init, sinon.match.instanceOf(Context), 'upgrade');
+            const chaincode = EmbeddedConnection.getChaincode('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+
+            // can't use deep equal here due to a cyclic object which causes a hang.
+            chaincode.uuid.should.equal('6eeb8858-eced-4a32-b1cd-2491f1e3718f');
+            chaincode.container.should.equal(mockContainer);
+            chaincode.engine.should.equal(mockEngine);
+            chaincode.installedBusinessNetwork.should.equal(mockInstalledBusinessNetwork);
+        });
+
+
     });
 
     describe('#ping', () => {
@@ -204,6 +297,11 @@ describe('EmbeddedConnection', () => {
     });
 
     describe('#queryChainCode', () => {
+        it('should throw an error if no business network defined', () => {
+            connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1');
+            connection.queryChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
+                .should.be.rejectedWith(/No business network/);
+        });
 
         it('should call the engine query method', async () => {
             // Mock a container
@@ -246,6 +344,13 @@ describe('EmbeddedConnection', () => {
     });
 
     describe('#invokeChainCode', () => {
+
+        it('should throw an error if no business network defined', () => {
+            connection = new EmbeddedConnection(mockConnectionManager, 'devFabric1');
+            connection.invokeChainCode(mockSecurityContext, 'testFunction', ['arg1', 'arg2'])
+                .should.be.rejectedWith(/No business network/);
+        });
+
 
         it('should call the engine invoke method', async () => {
 
@@ -371,10 +476,11 @@ describe('EmbeddedConnection', () => {
 
         it('should store a new identity if it does not exists', async () => {
             await connection._createAdminIdentity();
-            sinon.assert.calledTwice(mockIdentitiesDataCollection.add);
+            sinon.assert.calledOnce(mockIdentitiesDataCollection.add);
             mockIdentitiesDataCollection.add.getCall(0).args[0].should.equal('admin');
-            const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
+            //const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
             const adminIdentity1 = mockIdentitiesDataCollection.add.getCall(0).args[1];
+            const adminIdentityIdentifier = adminIdentity1.identifier;
             const certificateObj = new Certificate(adminIdentity1.certificate);
             certificateObj.getIdentifier().should.equal(adminIdentityIdentifier);
             certificateObj.getIssuer().should.equal('a3e3a2d42f1c55e1485c4d06ba8b5c64f83f697939346687b32bacaae5e38c8f');
@@ -386,8 +492,8 @@ describe('EmbeddedConnection', () => {
             adminIdentity1.secret.should.equal('adminpw');
             adminIdentity1.imported.should.be.false;
             adminIdentity1.options.issuer.should.be.true;
-            const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
-            adminIdentity1.should.deep.equal(adminIdentity2);
+            //const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
+            //adminIdentity1.should.deep.equal(adminIdentity2);
         });
 
     });
@@ -468,10 +574,11 @@ describe('EmbeddedConnection', () => {
             mockIdentitiesDataCollection.exists.withArgs('doge').resolves(false);
             mockIdentitiesDataCollection.add.withArgs('doge').resolves();
             const result = await connection.createIdentity(mockSecurityContext, 'doge');
-            sinon.assert.calledTwice(mockIdentitiesDataCollection.add);
+            sinon.assert.calledOnce(mockIdentitiesDataCollection.add);
             mockIdentitiesDataCollection.add.getCall(0).args[0].should.equal('doge');
-            const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
+            //const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
             const adminIdentity1 = mockIdentitiesDataCollection.add.getCall(0).args[1];
+            const adminIdentityIdentifier = adminIdentity1.identifier;
             const certificateObj = new Certificate(adminIdentity1.certificate);
             certificateObj.getIdentifier().should.equal(adminIdentityIdentifier);
             certificateObj.getIssuer().should.equal('a3e3a2d42f1c55e1485c4d06ba8b5c64f83f697939346687b32bacaae5e38c8f');
@@ -482,8 +589,8 @@ describe('EmbeddedConnection', () => {
             adminIdentity1.name.should.equal('doge');
             adminIdentity1.secret.should.equal('f892c30a');
             adminIdentity1.imported.should.be.false;
-            const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
-            adminIdentity1.should.deep.equal(adminIdentity2);
+            //const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
+            //adminIdentity1.should.deep.equal(adminIdentity2);
             result.should.be.deep.equal({ userID: 'doge', userSecret: 'f892c30a' });
         });
 
@@ -492,10 +599,11 @@ describe('EmbeddedConnection', () => {
             mockIdentitiesDataCollection.exists.withArgs('doge').resolves(false);
             mockIdentitiesDataCollection.add.withArgs('doge').resolves();
             const result = await connection.createIdentity(mockSecurityContext, 'doge', { issuer: true });
-            sinon.assert.calledTwice(mockIdentitiesDataCollection.add);
+            sinon.assert.calledOnce(mockIdentitiesDataCollection.add);
             mockIdentitiesDataCollection.add.getCall(0).args[0].should.equal('doge');
-            const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
+            //const adminIdentityIdentifier = mockIdentitiesDataCollection.add.getCall(1).args[0];
             const adminIdentity1 = mockIdentitiesDataCollection.add.getCall(0).args[1];
+            const adminIdentityIdentifier = adminIdentity1.identifier;
             const certificateObj = new Certificate(adminIdentity1.certificate);
             certificateObj.getIdentifier().should.equal(adminIdentityIdentifier);
             certificateObj.getIssuer().should.equal('a3e3a2d42f1c55e1485c4d06ba8b5c64f83f697939346687b32bacaae5e38c8f');
@@ -506,8 +614,8 @@ describe('EmbeddedConnection', () => {
             adminIdentity1.name.should.equal('doge');
             adminIdentity1.secret.should.equal('f892c30a');
             adminIdentity1.imported.should.be.false;
-            const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
-            adminIdentity1.should.deep.equal(adminIdentity2);
+            //const adminIdentity2 = mockIdentitiesDataCollection.add.getCall(1).args[1];
+            //adminIdentity1.should.deep.equal(adminIdentity2);
             result.should.be.deep.equal({ userID: 'doge', userSecret: 'f892c30a' });
         });
 
