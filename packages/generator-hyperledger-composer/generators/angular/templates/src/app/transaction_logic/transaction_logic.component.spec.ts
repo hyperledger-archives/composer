ce216809123dc65b71a3fad1853d35e3b90f8df6
Ahmed Nasser <ahmednasser@Sngv.local>
2018-06-10 15:11:01 +0200

    Add licence
    Signed-off-by: Ahmed Nasser <ahmednasser@Sngv.local>

--- packages/generator-hyperledger-composer/generators/angular/templates/src/app/transaction_logic/transaction_logic.component.spec.ts
+++ packages/generator-hyperledger-composer/generators/angular/templates/src/app/transaction_logic/transaction_logic.component.spec.ts
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
